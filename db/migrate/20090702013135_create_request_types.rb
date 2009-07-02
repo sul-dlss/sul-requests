e890187ec5a853121b60b60ccbe89e60647f2d2a
jlavigne <jlavigne>
2009-07-02 01:39:26 +0000

    More code for request type

--- db/migrate/20090702013135_create_request_types.rb
+++ db/migrate/20090702013135_create_request_types.rb
@@ -1,6 +1,13 @@
 class CreateRequestTypes < ActiveRecord::Migration
   def self.up
     create_table :request_types do |t|
+      t.string :type
+      t.string :current_loc
+      t.string :req_status
+      t.string :form
+      t.string :text
+      t.boolean :enabled
+      t.boolean :authenticated
 
       t.timestamps
     end
