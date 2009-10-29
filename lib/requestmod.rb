548b6f73c4f567c5e565e8f1e69b7d51d6cd281d
jlavigne <jlavigne>
2009-10-29 23:10:59 +0000

    Put join_hash method back into requestmod.rb

--- lib/requestmod.rb
+++ lib/requestmod.rb
@@ -388,5 +388,13 @@ module Requestmod
 
   end  # cleanup_field
   
+  # Method join_hash. Take a hash and return its elements joined by two delimiters
+  # Used to turn a params hash into a param string: key1=value1&key2=value2
+  def join_hash(hash, delim_1, delim_2)
+    keys = Array.new
+    hash.each {|a,b| keys << [a.to_s, b.to_s].join(delim_1)}
+    return keys.join(delim_2)
+  end    
+  
   
 end
