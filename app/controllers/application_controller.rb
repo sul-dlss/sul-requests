47dda249c0cae834870bf8d18cea9dbdc2e9d357
jlavigne <jlavigne>
2010-03-23 00:56:49 +0000

    Changes to start using rescue_fromm for exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
+  #TODO: Set up rescue_from for various exceptions
+  #rescue_from Exception, :with => :handle_exception
+  # See http://m.onkey.org/2008/7/20/rescue-from-dispatching for info about rescue_from
+  #rescue_from ActionController::RoutingError, :with => :route_not_found
+  
+
+
 
 
   # ================ Protected methods from here ====================
@@ -23,6 +30,22 @@ class ApplicationController < ActionController::Base
       end      
     end
   end 
+  
+  #TODO: Set up exception handling for various exceptions 
+  #TODO: Write exception text to a log
+  #def route_not_found(exception)
+  #  flash[:system_problem] = 'Page not found - 500 ' + exception
+  #  render :template => 'requests/app_problem', :status => :not_found
+  #end
+  
+  #TODO: Implement method to handle Exception
+  #def handle_exception(exception)
+  #  flash[:system_problem] = 'General application problem ' + exception
+  #  #Following gives a lot of useless info
+  #  logger.info exception.backtrace.join('\n')
+  #  render :template => 'requests/app_problem', :status => :not_found
+  #end
+
      
 
   # Scrub sensitive parameters from your log
