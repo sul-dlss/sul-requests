6b9cb734e5b32c250e97b52b8ccdebf3636f69bb
jlavigne <jlavigne>
2010-04-10 00:33:12 +0000

    Changes for exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,6 @@ class ApplicationController < ActionController::Base
   rescue_from Exception, :with => :handle_exception
   # See http://m.onkey.org/2008/7/20/rescue-from-dispatching for info about rescue_from
   #rescue_from ActionController::RoutingError, :with => :route_not_found
-  
-
-
 
 
   # ================ Protected methods from here ====================
@@ -42,15 +39,42 @@ class ApplicationController < ActionController::Base
   # Note that the exception statement here may give too much info for users
   # so just provide a general message and let the email/log give details
   def handle_exception(exception)
-    #TODO: Find a way to make the msg here reference msg 000. Just do a lookup??
-    flash[:system_problem] = 'There was an application problem that makes it impossible to process 
+    
+    case exception
+      when ActionController::RoutingError, ActionController::UnknownController, 
+        ActionController::UnknownAction
+      #when ActionController::NotImplemented
+      
+        proto = 'http://'
+        
+        if request.env['SSL']
+          proto = 'https://'
+        end
+        
+        flash[:system_problem] = "<p>Requested page: " + proto + request.env['HTTP_HOST'].to_str +
+          request.env['REQUEST_URI'].to_str + '</p>' +
+          'The page you requested was not found. If you entered this URL into your browser, ' +
+          'please check that it is correct. If you found the URL on a Web page, ' +
+          'notify the owner of the page about the problem.'
+        
+        render :template => 'requests/app_problem', :status => :not_found
+        
+      else  
+    
+        #TODO: Find a way to make the msg here reference msg 000. Just do a lookup??
+        flash[:system_problem] = 'There was an application problem that makes it impossible to process 
                               your request. We have sent a report about this problem.'
-    ExceptionMailer.deliver_exception_report(exception,
+    
+        ExceptionMailer.deliver_exception_report(exception,
           clean_backtrace(exception),
           session.instance_variable_get("@data"),
           params,
           request.env)
-    render :template => 'requests/app_problem', :status => :not_found
+          
+        render :template => 'requests/app_problem', :status => :not_found
+    
+    end # case
+    
   end
 
   
