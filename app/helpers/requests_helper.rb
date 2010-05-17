6d866ff0eb27fcdb4cef702615c11b630f3469ae
jlavigne <jlavigne>
2010-05-17 22:05:10 +0000

    Added CGI escaping to auth link

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,5 +1,7 @@
 module RequestsHelper
   
+  require 'cgi'
+  
   # Take a request instance variable and create a link to the auth'd version of the new form
   # The various values in the instance var come from the parms of the original request
   # Probably a better way to do this but could not get at contents of "request" object
@@ -15,27 +17,27 @@ module RequestsHelper
     # Add all the fields we have in this request
     
     if request.ckey != nil
-      link = link + 'ckey=' + request.ckey + '&'
+      link = link + 'ckey=' + CGI::escape(request.ckey) + '&'
     end
     
     if request.call_num != nil
-      link = link + 'call_num=' + request.call_num + '&' 
+      link = link + 'call_num=' + CGI::escape(request.call_num) + '&' 
     end
     
     if request.home_lib != nil
-      link = link + 'home_lib=' + request.home_lib + '&'
+      link = link + 'home_lib=' + CGI::escape(request.home_lib) + '&'
     end
     
     if request.current_loc != nil
-      link = link + 'current_loc=' + request.current_loc + '&' 
+      link = link + 'current_loc=' + CGI::escape(request.current_loc) + '&' 
     end
     
     if request.current_loc != nil
-      link = link + 'home_loc=' + request.home_loc + '&' 
+      link = link + 'home_loc=' + CGI::escape(request.home_loc) + '&' 
     end
     
     if request.item_id != nil
-      link = link + 'item_id=' + request.item_id + '&'
+      link = link + 'item_id=' + CGI::escape(request.item_id) + '&'
     end
     
     if request.not_needed_after != nil
@@ -47,7 +49,7 @@ module RequestsHelper
     end
     
     if request.req_type != nil
-      link = link + 'req_type=' + request.req_type + '&'
+      link = link + 'req_type=' + CGI::escape(request.req_type) + '&'
     end
     
     # Strip out the final &
