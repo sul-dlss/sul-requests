79c2c06eb0d202b5a1bbab1670e2d3158ae3c858
Jonathan Lavigne <jlavigne@stanford.edu>
2013-10-01 11:32:17 -0700

    Add "squider" to list of agents that shouldn't trigger e-mail for exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -73,7 +73,7 @@ class ApplicationController < ActionController::Base
         flash[:system_problem] = 'There was an application problem that makes it impossible to process 
                               your request. We have sent a report about this problem.'
     
-        if request.env['HTTP_USER_AGENT'] !~ /bot/i or request.env['HTTP_USER_AGENT'] !~ /spider/i or request.env['HTTP_USER_AGENT'] !~ /crawl/i or request.env['HTTP_USER_AGENT'] !~ /teoma/i
+        if request.env['HTTP_USER_AGENT'] !~ /bot/i or request.env['HTTP_USER_AGENT'] !~ /squider/i or request.env['HTTP_USER_AGENT'] !~ /spider/i or request.env['HTTP_USER_AGENT'] !~ /crawl/i or request.env['HTTP_USER_AGENT'] !~ /teoma/i
             ExceptionMailer.deliver_exception_report(exception,
             clean_backtrace(exception),
             session.instance_variable_get("@data"),
