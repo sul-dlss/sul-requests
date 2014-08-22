e9b7e6c68cbcafceaa0e1811380f848493bfd58a
Jessie Keck <jessie.keck@gmail.com>
2014-08-21 19:57:21 -0700

    Ask which branch to deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'symphony_requests'
 set :repo_url, 'https://github.com/sul-dlss/symphony_requests.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/requests/requests-app'
