a9957158c7d7241b3fe441a56530bd6b7a456cd1
Jessie Keck <jessie.keck@gmail.com>
2015-04-08 21:43:39 -0700

    Add methods to user class to identify when the user is in particular configured work groups.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,28 @@ class User < ActiveRecord::Base
   def webauth_user?
     webauth.present?
   end
+
+  def superadmin?
+    admin_groups = Settings.super_admin_groups || []
+    (ldap_groups & admin_groups).present?
+  end
+
+  def site_admin?
+    admin_groups = Settings.site_admin_groups || []
+    (ldap_groups & admin_groups).present?
+  end
+
+  def admin_for_origin?(origin)
+    admin_groups = Settings.origin_admin_groups[origin] || []
+    (ldap_groups & admin_groups).present?
+  end
+
+  def admin_for_destination?(destination)
+    admin_groups = Settings.destination_admin_groups[destination] || []
+    (ldap_groups & admin_groups).present?
+  end
+
+  def ldap_groups
+    (ENV['WEBAUTH_LDAPPRIVGROUP'] || '').split('|')
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,40 @@ describe User do
       expect(subject).to be_webauth_user
     end
   end
+  describe '#superadmin?' do
+    it 'should return false when the user is not a super admin' do
+      expect(subject).to_not be_superadmin
+    end
+    it 'should return true when the user is in a superadmin group' do
+      allow(subject).to receive_messages(ldap_groups: ['FAKE-TEST-SUPER-ADMIN-GROUP'])
+      expect(subject).to be_superadmin
+    end
+  end
+  describe '#site_admin?' do
+    it 'should return false when the user is not a site admin' do
+      expect(subject).to_not be_site_admin
+    end
+    it 'should return true when the user is in a site admin group' do
+      allow(subject).to receive_messages(ldap_groups: ['FAKE-TEST-SITE-ADMIN-GROUP'])
+      expect(subject).to be_site_admin
+    end
+  end
+  describe '#admin_for_origin?' do
+    it 'should return false when the user is not in an originating library admin group' do
+      expect(subject).to_not be_admin_for_origin('FAKE-ORIGIN-LIBRARY')
+    end
+    it 'should return true when the user is in a site admin group' do
+      allow(subject).to receive_messages(ldap_groups: ['FAKE-ORIGIN-LIBRARY-TEST-LDAP-GROUP'])
+      expect(subject).to be_admin_for_origin('FAKE-ORIGIN-LIBRARY')
+    end
+  end
+  describe '#admin_for_destination?' do
+    it 'should return false when the user is not in an destination library admin group' do
+      expect(subject).to_not be_admin_for_origin('FAKE-DESTINATION-LIBRARY')
+    end
+    it 'should return true when the user is in a site admin group' do
+      allow(subject).to receive_messages(ldap_groups: ['FAKE-DESTINATION-LIBRARY-TEST-LDAP-GROUP'])
+      expect(subject).to be_admin_for_destination('FAKE-DESTINATION-LIBRARY')
+    end
+  end
 end
