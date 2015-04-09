45d46652d85b5bad7668d04c914fc8dd5cb58a87
Jessie Keck <jessie.keck@gmail.com>
2015-04-08 21:44:33 -0700

    Add initial ability definitions for base request and page classes.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,32 +1,24 @@
+###
+#  Main ability class for authorization
+#  See the wiki for details about defining abilities:
+#  https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+###
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    user ||= User.new
+    can :manage, :all if user.superadmin?
+
+    can :manage, Request if user.site_admin?
+    # Adminstrators for origins or destinations should be able to
+    # manage requests originating or arriving to their library.
+    can :manage, Request do |request|
+      user.admin_for_origin?(request.origin) ||
+        user.admin_for_destination?(request.destination)
+    end
+
+    # Everyone can create Page Requests (for now).
+    can :create, Page
   end
 end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,111 @@
+require 'rails_helper'
+require 'cancan/matchers'
+
+describe Ability do
+  let(:request) { Request.new }
+  let(:custom) { Custom.new }
+  let(:hold_recall) { HoldRecall.new }
+  let(:mediated_page) { MediatedPage.new }
+  let(:page) { Page.new }
+  let(:scan) { Scan.new }
+  subject { Ability.new(user) }
+  describe 'an anonymous user' do
+    let(:user) { nil }
+
+    # Can create Page request.  Nothing else yet.
+    it { is_expected.to be_able_to(:create, page) }
+    it { is_expected.not_to be_able_to(:read, page) }
+    it { is_expected.not_to be_able_to(:update, page) }
+    it { is_expected.not_to be_able_to(:delete, page) }
+
+    it { is_expected.not_to be_able_to(:create, custom) }
+    it { is_expected.not_to be_able_to(:read, custom) }
+    it { is_expected.not_to be_able_to(:update, custom) }
+    it { is_expected.not_to be_able_to(:delete, custom) }
+
+    it { is_expected.not_to be_able_to(:create, hold_recall) }
+    it { is_expected.not_to be_able_to(:read, hold_recall) }
+    it { is_expected.not_to be_able_to(:update, hold_recall) }
+    it { is_expected.not_to be_able_to(:delete, hold_recall) }
+
+    it { is_expected.not_to be_able_to(:create, mediated_page) }
+    it { is_expected.not_to be_able_to(:read, mediated_page) }
+    it { is_expected.not_to be_able_to(:update, mediated_page) }
+    it { is_expected.not_to be_able_to(:delete, mediated_page) }
+
+    it { is_expected.not_to be_able_to(:create, scan) }
+    it { is_expected.not_to be_able_to(:read, scan) }
+    it { is_expected.not_to be_able_to(:update, scan) }
+    it { is_expected.not_to be_able_to(:delete, scan) }
+  end
+
+  describe 'a super admin' do
+    let(:user) { User.new }
+    before { allow(user).to receive_messages(superadmin?: true) }
+
+    # can manage anything
+    it { is_expected.to be_able_to(:manage, request) }
+    it { is_expected.to be_able_to(:manage, custom) }
+    it { is_expected.to be_able_to(:manage, hold_recall) }
+    it { is_expected.to be_able_to(:manage, mediated_page) }
+    it { is_expected.to be_able_to(:manage, page) }
+    it { is_expected.to be_able_to(:manage, scan) }
+  end
+
+  describe 'a site admin' do
+    let(:user) { User.new }
+    before { allow(user).to receive_messages(site_admin?: true) }
+
+    # can manage anything
+    it { is_expected.to be_able_to(:manage, request) }
+    it { is_expected.to be_able_to(:manage, custom) }
+    it { is_expected.to be_able_to(:manage, hold_recall) }
+    it { is_expected.to be_able_to(:manage, mediated_page) }
+    it { is_expected.to be_able_to(:manage, page) }
+    it { is_expected.to be_able_to(:manage, scan) }
+  end
+
+  describe 'an origin admin' do
+    let(:user) { User.new }
+
+    before do
+      allow(user).to receive_messages(ldap_groups: ['FAKE-ORIGIN-LIBRARY-TEST-LDAP-GROUP'])
+      request.origin = 'FAKE-ORIGIN-LIBRARY'
+      custom.origin = 'FAKE-ORIGIN-LIBRARY'
+      hold_recall.origin = 'FAKE-ORIGIN-LIBRARY'
+      mediated_page.origin = 'FAKE-ORIGIN-LIBRARY'
+      page.origin = 'FAKE-ORIGIN-LIBRARY'
+      scan.origin = 'FAKE-ORIGIN-LIBRARY'
+    end
+
+    # can manage locations that they are an admin of
+    it { is_expected.to be_able_to(:manage, request) }
+    it { is_expected.to be_able_to(:manage, custom) }
+    it { is_expected.to be_able_to(:manage, hold_recall) }
+    it { is_expected.to be_able_to(:manage, mediated_page) }
+    it { is_expected.to be_able_to(:manage, page) }
+    it { is_expected.to be_able_to(:manage, scan) }
+  end
+
+  describe 'a destination admin' do
+    let(:user) { User.new }
+
+    before do
+      allow(user).to receive_messages(ldap_groups: ['FAKE-DESTINATION-LIBRARY-TEST-LDAP-GROUP'])
+      request.destination = 'FAKE-DESTINATION-LIBRARY'
+      custom.destination = 'FAKE-DESTINATION-LIBRARY'
+      hold_recall.destination = 'FAKE-DESTINATION-LIBRARY'
+      mediated_page.destination = 'FAKE-DESTINATION-LIBRARY'
+      page.destination = 'FAKE-DESTINATION-LIBRARY'
+      scan.destination = 'FAKE-DESTINATION-LIBRARY'
+    end
+
+    # can manage locations that they are an admin of
+    it { is_expected.to be_able_to(:manage, request) }
+    it { is_expected.to be_able_to(:manage, custom) }
+    it { is_expected.to be_able_to(:manage, hold_recall) }
+    it { is_expected.to be_able_to(:manage, mediated_page) }
+    it { is_expected.to be_able_to(:manage, page) }
+    it { is_expected.to be_able_to(:manage, scan) }
+  end
+end
