275c08f748825f5fff9d1757b9d888eaf2cb9f3d
Jessie Keck <jessie.keck@gmail.com>
2015-04-09 23:24:55 -0700

    touch tmp/restart.txt after publishing the deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,14 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -46,5 +54,4 @@ namespace :deploy do
       # end
     end
   end
-
 end
