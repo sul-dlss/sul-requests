fcae68482f4a5111189c9c5bec135fbbb69e55ee
Jessie Keck <jessie.keck@gmail.com>
2015-04-17 16:43:11 -0700

    Add helper to return a dropdown of pickup libraries.

---
+++ spec/features/pickup_libraries_dropdown_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Pickup Libraries Dropdown' do
+  describe 'for multiple libraries' do
+    it 'should have a select dropdown to choose the library to deliver to' do
+      visit new_request_path(item_id: '1234', origin: 'GREEN', origin_location: 'STACKS')
+
+      expect(page).to have_select('Deliver to')
+
+      expect(page).to have_css('#page_destination option', count: 15)
+    end
+
+    it 'should simply display the text of the destination library if there is only one possible' do
+      visit new_request_path(item_id: '1234', origin: 'ARS', origin_location: 'STACKS')
+
+      expect(page).to_not have_css('select')
+
+      expect(page).to have_css('.form-group .control-label', text: 'Must be used in')
+      expect(page).to have_css('.form-group .input-like-text', text: 'Archive of Recorded Sound')
+    end
+  end
+end
---
+++ spec/helpers/requests_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe RequestsHelper do
+  describe '#select_for_pickup_libraries' do
+    let(:form) { double('form') }
+    before do
+      allow(form).to receive_messages(object: request)
+    end
+    describe 'single library' do
+      let(:request) { Request.new(origin: 'SAL3', origin_location: 'PAGE-MU') }
+      it 'should return library text and a hidden input w/ the destination library' do
+        expect(form).to receive(:hidden_field).with(:destination, value: 'MUSIC').and_return('<hidden_field>')
+        markup = Capybara.string(select_for_pickup_libraries(form))
+        expect(markup).to have_css('.form-group .control-label', text: 'Must be used in')
+        expect(markup).to have_css('.form-group .input-like-text', text: 'Music Library')
+        expect(markup).to have_css('hidden_field')
+      end
+    end
+    describe 'multiple libraries' do
+      let(:request) { Request.new(origin: 'SAL3', origin_location: 'PAGE-HP') }
+      it 'should attempt to create a select list' do
+        expect(form).to receive(:select).with(
+          :destination,
+          [['Green Library', 'GREEN'], ['Marine Biology Library (Miller)', 'HOPKINS']],
+          label: 'Deliver to'
+        ).and_return('<select>')
+        expect(select_for_pickup_libraries(form)).to eq '<select>'
+      end
+    end
+  end
+end
