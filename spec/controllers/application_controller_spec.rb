cf283d88a44d992aea65d38ea1f70e47176647dd
Jessie Keck <jessie.keck@gmail.com>
2015-04-16 20:58:17 -0700

    Have current user return a blank new user object when there is no webauth user.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 describe ApplicationController do
   describe '#current_user' do
-    it 'should return nil if there is no user in the environment' do
-      expect(controller.send(:current_user)).to be_nil
+    it 'should return nil user if there is no user in the environment' do
+      user = controller.send(:current_user)
+      expect(user).to be_a User
+      expect(user).to_not be_persisted
+      expect(user.id).to be_nil
     end
     it 'should return a user when there is a user in the environment' do
       allow(controller).to receive_messages(user_id: 'some-user')
