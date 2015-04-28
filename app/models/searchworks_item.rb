85de3a5068dcad65a42353e54677740a555af937
Jessie Keck <jessie.keck@gmail.com>
2015-04-28 15:59:19 -0700

    Modify SearchworksItem class to use new availability JSON.

--- app/models/searchworks_item.rb
+++ app/models/searchworks_item.rb
@@ -1,5 +1,5 @@
 ###
-#  Class to connect to the searchworks API at http://searchworks.stanford.edu/view/#{ckey}.mobile?covers=false
+#  Class to connect to the searchworks availability API
 #  The API URI is configured using rails_config: Settings.searchworks_api
 ###
 class SearchworksItem
@@ -8,36 +8,29 @@ class SearchworksItem
   end
 
   def title
-    item_xml['title'] || ''
+    json['title'] || ''
+  end
+
+  def holdings
+    return [] unless json['holdings'].present?
+    @holdings ||= JSON.parse(json['holdings'].to_json, object_class: OpenStruct)
   end
 
   private
 
-  def item_xml
-    response_xml['LBItem'] || {}
-  end
-
-  def response_xml
-    xml_hash['response'] || {}
-  end
-
-  def uri
+  def base_uri
     Settings.searchworks_api
   end
 
   def url
-    "#{uri}/view/#{@item_id}.mobile?covers=false"
+    [base_uri, 'view', @item_id, 'availability'].join('/')
   end
 
   def response
     @response ||= Faraday.get(url)
   end
 
-  def xml
-    response.body
-  end
-
-  def xml_hash
-    @xml_hash ||= Hash.from_xml(xml)
+  def json
+    @json ||= JSON.parse(response.body) || {}
   end
 end
