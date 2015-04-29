398c71429e1247d9347750c089ad281c3ef27d6a
Chris Jesudurai <jchristo4@gmail.com>
2015-04-29 15:18:15 -0700

    Adds title to admin UI

--- spec/features/admin_requests_spec.rb
+++ spec/features/admin_requests_spec.rb
@@ -4,8 +4,8 @@ describe 'Viewing all requests' do
   describe 'by a superadmin user' do
     before do
       stub_current_user(create(:superadmin_user))
-      create(:page, item_id: 2345)
-      create(:page, item_id: 2346, origin: 'SAL-NEWARK')
+      create(:page, item_id: 2345, item_title: 'Fourth symphony. [Op. 51].')
+      create(:page, item_id: 2346, item_title: 'An American in Paris', origin: 'SAL-NEWARK')
     end
     it 'should list data in tables' do
       visit admin_index_path
@@ -13,12 +13,14 @@ describe 'Viewing all requests' do
       expect(page).to have_css('h3', text: /Green Library/)
       expect(page).to have_css('h4', text: /Library=GREEN & location=STACKS/)
       expect(page).to have_css('td', text: '2345')
+      expect(page).to have_css('td', text: /Fourth symphony. \[Op. 51\]./)
 
       expect(page).to have_css('h3', text: /SAL Newark/)
       expect(page).to have_css('h4', text: /Library=SAL-NEWARK & location=STACKS/)
       expect(page).to have_css('td', text: '2346')
+      expect(page).to have_css('td', text: /An American in Paris/)
 
-      expect(page).to have_selector('td', text: 'Page', count: 2)
+      expect(page).to have_selector('td', text: /^Page$/, count: 2)
       expect(page).to have_selector('table.table-striped', count: 2)
     end
   end
