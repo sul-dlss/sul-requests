09555005f939a3abcce61bd46092f0deb38343d5
Jessie Keck <jessie.keck@gmail.com>
2015-05-08 11:50:27 -0700

    Redirect to the home page after logout since the user may be at a restricted location.

--- spec/views/shared/_top_navbar.html.erb_spec.rb
+++ spec/views/shared/_top_navbar.html.erb_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 describe 'shared/_top_navbar.html.erb' do
-  it 'should have a login link if there is no user' do
-    expect(view).to receive_messages(current_user: create(:anon_user))
+  before do
+    expect(view).to receive_messages(current_user: user)
     render
-    expect(rendered).to have_css('a', text: 'Login')
   end
-  it 'should have a logout link if there is a user' do
-    expect(view).to receive_messages(current_user: create(:webauth_user))
-    render
-    expect(rendered).to have_css('a', text: 'some-webauth-user: Logout')
+  describe 'login link' do
+    let(:user) { create(:anon_user) }
+    it 'should have a login link if there is no user' do
+      expect(rendered).to have_css('a', text: 'Login')
+    end
+  end
+
+  describe 'logout link' do
+    let(:user) { create(:webauth_user) }
+    it 'should have a logout link if there is a user' do
+      expect(rendered).to have_css('a', text: 'some-webauth-user: Logout')
+    end
+    it 'should redirect users back to the home page of the app' do
+      expect(rendered).to have_css('a[href="/webauth/logout?referrer=%2F"]')
+    end
   end
 end
