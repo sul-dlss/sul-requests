a62f69476de73432e2d770e21da71f7a8dababa9
Jessie Keck <jessie.keck@gmail.com>
2015-06-11 14:42:55 -0700

    Add js module for the item selector and two separate modules for updating the item count and enforcing selection limit.

---
+++ app/assets/javascripts/item_selector/item_selector_incrementor.js
@@ -0,0 +1,51 @@
+var itemSelectorIncrementor = (function() {
+  return $.extend(itemSelector, {
+    init: function() {
+      var _this = this;
+      $(document).on('ready page:load', function(){
+        _this.setDefaultItemCounter();
+        _this.addIncrementBehavior();
+      });
+    },
+
+    addIncrementBehavior: function() {
+      var _this = this;
+      _this.checkboxes().each(function() {
+        $(this).on('item-selector:selected', function() {
+          _this.increaseSelectedItemCount();
+        });
+
+        $(this).on('item-selector:deselected', function() {
+          _this.decreaseSelectedItemCount();
+        });
+      });
+    },
+
+    increaseSelectedItemCount: function() {
+      this.selectedItemCounter().text(
+        parseInt(this.selectedItemCounter().text(), 10) + 1
+      );
+    },
+
+    decreaseSelectedItemCount: function() {
+      this.selectedItemCounter().text(
+        parseInt(this.selectedItemCounter().text(), 10) - 1
+      );
+    },
+
+    setDefaultItemCounter: function() {
+      this.selectedItemCounter().text(
+        this.numberOfSelectedCheckboxes()
+      );
+    },
+
+    selectedItemCounter: function() {
+      var counterSelector = this.selectorElement()
+                                .find('[data-counter-target]')
+                                .data('counter-target');
+      return $(counterSelector).find('[data-count]');
+    }
+  });
+})();
+
+itemSelectorIncrementor.init();
