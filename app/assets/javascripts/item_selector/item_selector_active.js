df9bd4424b1e5e07b733bbd15c76dd6fe0612cdb
Chris Beer <cabeer@stanford.edu>
2015-07-14 09:32:31 -0700

    Style the item selector widget; fixes #196

---
+++ app/assets/javascripts/item_selector/item_selector_active.js
@@ -0,0 +1,44 @@
+var itemSelectorActive = (function() {
+
+  return $.extend(itemSelector, {
+    init: function() {
+      var _this = this;
+      $(document).on('ready page:load', function(){
+        _this.setupDefaultActiveRows();
+        _this.addActiveBehavior();
+      });
+    },
+
+    setupDefaultActiveRows: function() {
+      var _this = this;
+      _this.checkboxes()
+           .filter(':checked')
+           .each(function() {
+             _this.addActive($(this));
+           });
+    },
+
+    addActiveBehavior: function() {
+      var _this = this;
+      _this.selectorElement()
+           .on('item-selector:selected', function(event, item) {
+             _this.addActive(item);
+      });
+
+      _this.selectorElement()
+           .on('item-selector:deselected', function(event, item) {
+             _this.removeActive(item);
+      });
+    },
+
+    addActive: function(item) {
+      item.closest('.input-group').addClass('active');
+    },
+
+    removeActive: function(item) {
+      item.closest('.input-group').removeClass('active');
+    }
+  });
+})();
+
+itemSelectorActive.init();
