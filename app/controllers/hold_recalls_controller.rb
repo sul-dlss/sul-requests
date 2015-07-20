41b94967d80dafa9247a17be2a0f4f5e08d1016a
Jessie Keck <jessie.keck@gmail.com>
2015-07-20 10:57:26 -0700

    Set default hold/recall needed_date to 1 year from today on new and create actions.

--- app/controllers/hold_recalls_controller.rb
+++ app/controllers/hold_recalls_controller.rb
@@ -2,7 +2,13 @@
 #  Controller to handle particular behaviors for HoldRecall type requests
 ###
 class HoldRecallsController < RequestsController
+  before_action :set_needed_date, only: [:new, :create]
+
   protected
+
+  def set_needed_date
+    current_request.needed_date ||= Time.zone.today + 1.year
+  end
 
   def validate_request_type
     fail NotHoldRecallableError unless current_request.hold_recallable?
