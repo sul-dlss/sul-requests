94c887bad20aea6f92fc0ad867cf20fd40c236c7
Chris Beer <cabeer@stanford.edu>
2015-09-03 12:03:33 -0700

    Make Request#holdings work with a request without selected barcoded holdings; fixes #388

--- app/models/concerns/holdings.rb
+++ app/models/concerns/holdings.rb
@@ -7,14 +7,14 @@ module Holdings
   end
 
   def holdings
-    return requested_holdings if requested_holdings.present?
+    return requested_holdings if persisted? || requested_holdings.present?
     holdings_object.all
   end
 
   private
 
   def requested_holdings
-    return unless requested_barcode.present? || barcodes.present?
+    return [] unless requested_barcode.present? || barcodes.present?
     holdings_object.where(barcodes: requested_barcode || barcodes)
   end
 end
