75be55a4cb39e2c550bfcbdfbf4f8b3717c8f88f
Jessie Keck <jessie.keck@gmail.com>
2015-09-10 17:18:42 -0700

    Send contentHeight and successPage boolean via postMessage to the parent frame.

---
+++ app/assets/javascripts/postMessage.js
@@ -0,0 +1,25 @@
+var iframePostMessage = (function() {
+  return {
+    init: function() {
+      $(document).on('ready page:load', function(){
+        if (top != self) {
+          setInterval(function(){
+            var message = {
+              contentHeight: $('html').outerHeight(true),
+              successPage: $('.success-page').length > 0
+            };
+
+            // If we ever extend the message to include something other than
+            // contentHeight or the successPage boolean then we should replace
+            // '*' with the origin of who we will allow our messages to be sent
+            // to. As it stands the data we are sending isn't sensitive and
+            // can't be used to identify the user in any way.
+            parent.postMessage(message, '*');
+          }, 1000);
+        }
+      });
+    }
+  };
+})();
+
+iframePostMessage.init();
