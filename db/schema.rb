0a810b55d4d7debdcfcb2f24133775ab19fbff6a
Jessie Keck <jessie.keck@gmail.com>
2015-09-29 23:12:45 -0700

    Store estimated date and display on status/success pages.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901152211) do
+ActiveRecord::Schema.define(version: 20150929234049) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "text"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20150901152211) do
     t.string   "type"
     t.date     "needed_date"
     t.string   "status"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "origin"
     t.string   "destination"
     t.string   "origin_location"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150901152211) do
     t.text     "data"
     t.string   "item_title"
     t.text     "barcodes"
+    t.string   "estimated_delivery"
   end
 
   add_index "requests", ["needed_date"], name: "index_requests_on_needed_date"
--- spec/features/paging_schedule_spec.rb
+++ spec/features/paging_schedule_spec.rb
@@ -28,6 +28,28 @@ describe 'Paging Schedule' do
     end
   end
 
+  describe 'Estimated delivery', js: true do
+    before do
+      stub_current_user(create(:webauth_user))
+      stub_searchworks_api_json(build(:sal3_holdings))
+    end
+    it 'is persisted' do
+      visit new_page_path(item_id: '1234', origin: 'SAL3', origin_location: 'STACKS')
+
+      expect(page).to have_css('[data-scheduler-text]', text: /, (before|after)/, visible: true)
+      schedule_text = find('[data-scheduler-text]').text
+
+      within('#item-selector') do
+        check('ABC 123')
+      end
+
+      first(:button, 'Send request').click
+
+      expect(page).to have_css('dt', text: /Estimated delivery/i)
+      expect(page).to have_css('dd', text: schedule_text)
+    end
+  end
+
   describe 'Single library destination', js: true do
     it 'displays an estimate for the single possible destination' do
       visit new_page_path(item_id: '1234', origin: 'SAL3', origin_location: 'PAGE-MA')
