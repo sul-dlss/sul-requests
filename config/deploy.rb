643746eace47b0cd71e538420e19b7952ab2d8df
Naomi Dushay <ndushay@stanford.edu>
2016-10-04 18:26:33 -0700

    update gems used for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,23 +54,3 @@ Capistrano::OneTimeKey.generate_one_time_key!
 # set :keep_releases, 5
 
 before 'deploy:publishing', 'squash:write_revision'
-
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-end
