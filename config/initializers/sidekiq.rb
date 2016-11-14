bc46a395c204b61eb6c422f29d05995c7ea01d3e
Tommy Ingulfsen <tommyi@stanford.edu>
2016-11-14 15:38:26 -0800

    Clean up Sidekiq initializer.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,5 @@
 if Settings.background_jobs && Settings.background_jobs.enabled
   Sidekiq.configure_client do |config|
-    Sidekiq::Logging.initialize_logger(Settings.background_jobs.logfile) if Settings.background_jobs.logfile
     Sidekiq.logger.level = Settings.background_jobs.log_level.constantize if Settings.background_jobs.log_level
   end
 end
