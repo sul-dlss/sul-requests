14aede5730ac59e0669525554f5681df75541203
Jessie Keck <jessie.keck@gmail.com>
2018-06-04 23:29:47 -0700

    Add a failing spec to exercise #757

--- spec/features/create_hold_recall_spec.rb
+++ spec/features/create_hold_recall_spec.rb
@@ -62,4 +62,29 @@ describe 'Creating a hold recall request' do
       expect(HoldRecall.last.barcodes).to eq ['12345678']
     end
   end
+
+  describe 'by a user who had a sunet ID but now uses their libarry ID', js: true do
+    before do
+      user.update(library_id: '123456')
+    end
+
+    it 'redirects to the success page appropriately' do
+      visit new_hold_recall_path(
+        item_id: '1234',
+        barcode: '12345678',
+        origin: 'SAL3',
+        origin_location: 'STACKS'
+      )
+
+      click_link "I don't have a SUNet ID"
+
+      fill_in 'Library ID', with: '123456'
+
+      click_button 'Send request'
+
+      expect(HoldRecall.last.user).to eq User.last
+      expect(User.last.library_id).to eq '123456'
+      expect_to_be_on_success_page
+    end
+  end
 end
