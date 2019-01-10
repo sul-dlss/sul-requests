aa5083cc35dd170071ada2934287b95cb5af2c79
Jessie Keck <jessie.keck@gmail.com>
2019-01-10 11:32:00 -0800

    Version migration (was originally written in rails 4)

--- db/migrate/20181115194822_add_borrow_direct_boolean_column_to_requests.rb
+++ db/migrate/20181115194822_add_borrow_direct_boolean_column_to_requests.rb
@@ -1,4 +1,4 @@
-class AddBorrowDirectBooleanColumnToRequests < ActiveRecord::Migration
+class AddBorrowDirectBooleanColumnToRequests < ActiveRecord::Migration[4.2]
   def change
     add_column :requests, :via_borrow_direct, :boolean, default: false
   end
