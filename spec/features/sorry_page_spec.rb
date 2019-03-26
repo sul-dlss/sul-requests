1a0c12879175e2979d081103b90ea8510974125c
Joshua Greben <jgreben@stanford.edu>
2019-03-26 11:40:48 -0700

    Update feature spec with contact info changes

--- spec/features/sorry_page_spec.rb
+++ spec/features/sorry_page_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe 'Viewing sorry page' do
   it 'displays some text' do
     visit sorry_unable_path
-    expect(page).to have_css('p', text: 'greencirc@stanford.edu')
-    expect(page).to have_css('p', text: '(650) 723-1493')
+    expect(page).to have_css('p', text: 'scan-and-deliver@stanford.edu')
+    expect(page).to have_css('p', text: '(650) 723-3278')
   end
 end
