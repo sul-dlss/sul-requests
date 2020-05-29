deb2128557266c6f611f0ee1cf415c313716d315
Jessie Keck <jessie.keck@gmail.com>
2020-05-28 21:08:15 -0700

    Don't collect/require needed_date for any requests.

--- spec/features/library_instructions_spec.rb
+++ spec/features/library_instructions_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 describe 'Library Instructions' do
   before { stub_searchworks_api_json(build(:library_instructions_holdings)) }
 
-  it 'returns the library instructions from the API' do
+  # TODO: COVID-19 We don't render the needed_date (where these instructions are rendered)
+  pending 'returns the library instructions from the API' do
     visit new_request_path(item_id: '12345', origin: 'SPEC-COLL', origin_location: 'STACKS')
 
     within('p.needed-date-info-block') do
