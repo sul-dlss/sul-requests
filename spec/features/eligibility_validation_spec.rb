6fb5fad59ec7970e612c0c2e0004186fb630afc7
Jessie Keck <jessie.keck@gmail.com>
2020-06-04 18:18:43 -0700

    Allow users who can manage mediated pages to submit them, even if they don't have the appropriate affiliation.

--- spec/features/eligibility_validation_spec.rb
+++ spec/features/eligibility_validation_spec.rb
@@ -85,5 +85,23 @@ describe 'Eligibility Validation' do
         expect(MediatedPage.last).to be_nil
       end
     end
+
+    context 'when the user as an ineligible affiliation but can manage the request' do
+      before do
+        expect(Settings).to receive(:origin_admin_groups).and_return(
+          'SPEC-COLL' => ['sul:spec-coll-test-admins']
+        )
+        user.affiliation = 'stanford:staff'
+        user.ldap_group_string = 'sul:spec-coll-test-admins'
+      end
+
+      it 'allows users who can manage the mediated page to submit the request ' do
+        visit new_mediated_page_path(item_id: '1234', origin: 'SPEC-COLL', origin_location: 'STACKS')
+        first(:button, 'Send request').click
+
+        expect(current_url).to eq successful_mediated_page_url(MediatedPage.last)
+        expect_to_be_on_success_page
+      end
+    end
   end
 end
