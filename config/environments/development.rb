ebcf89bda1a271841fb4202903f00bd5010590e1
Jessie Keck <jessie.keck@gmail.com>
2020-07-21 16:05:11 -0700

    Add our development host since it runs in the rails dev env.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Since our dev server runs in the rails "development" env, we need to add our host
+  config.hosts << 'requests-dev.stanford.edu'
+
   # Do not eager load code on boot.
   config.eager_load = false
 
