6eb476ef0f9faa731e89332e5d6f42f1cc6003cd
Chris Beer <cabeer@stanford.edu>
2020-08-21 11:38:43 -0700

    Only trigger picklists emails in -prod

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,6 @@
 set :rails_env, 'production'
 
-server 'requests-prod.stanford.edu', user: 'requests', roles: %w(web db app)
+server 'requests-prod.stanford.edu', user: 'requests', roles: %w(web db app production_cron)
 
 set :bundle_without, %w{deployment development test}.join(' ')
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
-every '30 11 * * 1-5' do
+every '30 11 * * 1-5', roles: :production_cron do
   runner 'PicklistMailer.deliver_picklist "SPEC-COLL"'
 end
 
-every '30 15 * * 1-5' do
+every '30 15 * * 1-5', roles: :production_cron do
   runner 'PicklistMailer.deliver_picklist "SPEC-COLL"'
 end
