95b7e1db74f4f285b256b1aca9f338281130da90
Jessie Keck <jessie.keck@gmail.com>
2020-09-02 17:26:12 -0700

    Begin to add plumbing for renewing an item.

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
 
   get '/cdl/checkin' => 'cdl#checkin'
   get '/cdl/checkout' => 'cdl#checkout'
+  get '/cdl/renew' => 'cdl#renew'
   get '/cdl/availability/:barcode' => 'cdl#availability'
 
   concern :creatable_via_get_redirect do
