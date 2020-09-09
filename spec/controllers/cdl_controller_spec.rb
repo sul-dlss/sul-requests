342eeed6c5e20e9feb042e0a2e56ce5e2c378bb2
Jack Reed <phillipjreed@gmail.com>
2020-09-09 10:50:33 -0600

    Add some controller tests around CDL checkin/checkout

--- spec/controllers/cdl_controller_spec.rb
+++ spec/controllers/cdl_controller_spec.rb
@@ -9,4 +9,84 @@ describe CdlController do
       expect(response).to be_successful
     end
   end
+
+  describe '#checkin' do
+    context 'public access' do
+      it 'is restricted' do
+        get :checkin, params: { hold_record_key: 'abc123' }
+        expect(response).to redirect_to(
+          '/webauth/login?referrer=http%3A%2F%2Ftest.host%2Fcdl%2Fcheckin%3Fhold_record_key%3Dabc123'
+        )
+      end
+    end
+
+    context 'webauth user' do
+      before do
+        allow(controller).to receive_messages(current_user: user)
+        allow(user).to receive_messages(patron: Patron.new(patron_record))
+      end
+
+      let(:user) { create(:webauth_user) }
+      let(:patron_record) do
+        {
+          'fields' => {
+            'holdRecordList' => [
+              {
+                'key' => 'abc123',
+                'fields' => {
+                  'comment' => 'CDL;'
+                }
+              }
+            ]
+          }
+        }
+      end
+
+      it 'redirects when successful' do
+        get :checkin, params: { hold_record_key: 'abc123', return_to: 'http://example.com' }
+        expect(response).to redirect_to(
+          'http://example.com?success=true'
+        )
+      end
+    end
+  end
+
+  describe '#checkout' do
+    context 'public access' do
+      it 'is restricted' do
+        get :checkout, params: { id: 'ab123cd4567', barcode: '123456' }
+        expect(response).to redirect_to(
+          '/webauth/login?referrer=http%3A%2F%2Ftest.host%2Fcdl%2Fcheckout%3Fbarcode%3D123456%26id%3Dab123cd4567'
+        )
+      end
+    end
+
+    context 'webauth user' do
+      before do
+        allow(controller).to receive_messages(current_user: user)
+        allow(user).to receive_messages(patron: Patron.new(patron_record))
+      end
+
+      let(:user) { create(:webauth_user) }
+      let(:patron_record) do
+        {
+          'fields' => {
+            'holdRecordList' => [
+              {
+                'key' => 'abc123',
+                'fields' => {
+                  'comment' => 'CDL;'
+                }
+              }
+            ]
+          }
+        }
+      end
+
+      it 'if not token exists render success' do
+        get :checkout, params: { id: 'ab123cd4567', barcode: '123456' }
+        expect(response).to be_successful
+      end
+    end
+  end
 end
