4c00de23d1fc12949034daca6b1444ca0e2eb5fd
Jack Reed <phillipjreed@gmail.com>
2020-09-14 10:56:08 -0600

    Update requests-dev to use RAILS_ENV=production

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,7 +1,7 @@
-set :rails_env, 'development'
+set :rails_env, 'production'
 
 server 'requests-dev.stanford.edu', user: 'requests', roles: %w(web db app)
 
-set :bundle_without, %w{deployment test}.join(' ')
+set :bundle_without, %w{deployment development test}.join(' ')
 
 Capistrano::OneTimeKey.generate_one_time_key!
