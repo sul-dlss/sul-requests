9e2890a87f25f42bef5d04c48ef26abbf484e1a3
Chris Beer <cabeer@stanford.edu>
2020-10-13 07:22:30 -0700

    Use the :test adapter for jobs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,8 @@ module DisallowAPIs
   end
 end
 
+ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
