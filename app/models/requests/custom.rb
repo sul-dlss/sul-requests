3f33878cdf5f8037d058a74cac1ed74e25e861c5
Chris Beer <chris@cbeer.info>
2022-05-05 09:35:35 -0700

    Remove unused Custom model

--- app/models/requests/custom.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-###
-#  Request class for making custom requests
-#  that don't have any related item information
-###
-class Custom < Request
-end
--- spec/models/requests/custom_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-describe Custom do
-  it 'has the properly assigned Rails STI attribute value' do
-    expect(subject.type).to eq 'Custom'
-  end
-end
