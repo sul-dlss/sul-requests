57cfed1e378435efd4f8194c236ba9be7b43a131
Chris Beer <chris@cbeer.info>
2022-07-05 07:56:34 -0700

    Appease rubocop

--- app/services/symphony_client.rb
+++ app/services/symphony_client.rb
@@ -161,21 +161,21 @@ class SymphonyClient
     response = authenticated_request(
       '/circulation/circRecord/checkOut',
       method: :post, params: {
-                       includeFields: 'circRecord{*,item{barcode}}'
-                     }, json: {
-                       itemBarcode: item_barcode,
-                       # patronBarcode: patron_barcode
-                       # Using patron key here instead of barcode due to a Symphony
-                       # Web Services bug where the transaction is not reported in
-                       # statistics logs.
-                       patron: {
-                         resource: '/user/patron',
-                         key: Settings.cdl.pseudo_patron_key
-                       }
-                     }.merge(params), headers: {
-                       'SD-Prompt-Return': sd_prompt_return.join(';'),
-                       'SD-Working-LibraryID': 'SUL'
-                     }
+        includeFields: 'circRecord{*,item{barcode}}'
+      }, json: {
+        itemBarcode: item_barcode,
+        # patronBarcode: patron_barcode
+        # Using patron key here instead of barcode due to a Symphony
+        # Web Services bug where the transaction is not reported in
+        # statistics logs.
+        patron: {
+          resource: '/user/patron',
+          key: Settings.cdl.pseudo_patron_key
+        }
+      }.merge(params), headers: {
+        'SD-Prompt-Return': sd_prompt_return.join(';'),
+        'SD-Working-LibraryID': 'SUL'
+      }
     )
     begin
       JSON.parse(response.body)
@@ -208,17 +208,17 @@ class SymphonyClient
       "CIRC_UNSEEN_RENEW_LIMIT_OVRCD/#{Settings.symphony.override}"
     ]
     response = authenticated_request('/circulation/circRecord/renew', method: :post, params: {
-                                                                                       includeFields: 'circRecord{*,item{barcode}}'
-                                                                                     }, json: {
-                                                                                          item: {
-                                                                                            key: item_barcode,
-                                                                                            resource: '/catalog/item'
-                                                                                          },
-                                                                                          patronBarcode: patron_barcode, **params
-                                                                                        }, headers: {
-                                                                                          'SD-Prompt-Return': sd_prompt_return.join(';'),
-                                                                                          'SD-Working-LibraryID': 'SUL'
-                                                                                        })
+                                       includeFields: 'circRecord{*,item{barcode}}'
+                                     }, json: {
+                                       item: {
+                                         key: item_barcode,
+                                         resource: '/catalog/item'
+                                       },
+                                       patronBarcode: patron_barcode, **params
+                                     }, headers: {
+                                       'SD-Prompt-Return': sd_prompt_return.join(';'),
+                                       'SD-Working-LibraryID': 'SUL'
+                                     })
     begin
       JSON.parse(response.body)
     rescue JSON::ParserError
