1f6532878bd0d5a44a77a47dbd565856da1adc35
Justin Coyne <jcoyne@justincoyne.com>
2023-01-11 12:19:18 -0600

    Add log file for rspec status

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ Capybara.register_driver :headless_chrome do |app|
   end
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
-Capybara.default_max_wait_time = 30
+Capybara.default_max_wait_time = 5
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
