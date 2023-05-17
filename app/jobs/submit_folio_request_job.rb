c614832184c6a9b9e34124d1172bbd017de49120
Chris Beer <chris@cbeer.info>
2023-05-17 14:31:26 -0700

    Stub out the FOLIO request job

--- app/jobs/submit_folio_request_job.rb
+++ app/jobs/submit_folio_request_job.rb
@@ -13,17 +13,45 @@ class SubmitFolioRequestJob < ApplicationJob
 
     return true unless request
 
-    # TODO: something like this
-    # response = Call folio here
-    # request.merge_folio_response_data(FolioResponse.new(response.with_indifferent_access))
-    # request.save
-    # request.send_approval_status!
-    logger.info("NOOP FolioRequest request #{request_id}")
+    Sidekiq.logger.info("Started SubmitFolioRequestJob for request #{request_id}")
+    response = Command.new(request, **options).execute!
+
+    Sidekiq.logger.debug("FOLIO response: #{response}")
+    request.merge_ils_response_data(FolioResponse.new(response.with_indifferent_access))
+    request.save
+    request.send_approval_status!
+    Sidekiq.logger.info("Completed SubmitFolioRequestJob for request #{request_id}")
   end
 
   def find_request(request_id)
     Request.find(request_id)
   rescue ActiveRecord::RecordNotFound
     Honeybadger.notify('Unable to find Request', conext: { request_id: request_id })
+  end
+
+  # Submit a hold request to FOLIO
+  class Command
+    attr_reader :request, :folio_client, :barcode
+
+    delegate :user, to: :request
+    delegate :patron, to: :user
+
+    def initialize(request, folio_client: nil, barcode: nil)
+      @request = request
+      @folio_client = folio_client || FolioClient.new
+      @barcode = barcode
+    end
+
+    def execute!
+      {}
+    end
+
+    def request_params
+      {}
+    end
+  end
+
+  def self.command
+    Command
   end
 end
