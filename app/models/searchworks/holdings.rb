b165fdd341d4b6d6ca707089458ac112cf254bd0
Justin Coyne <jcoyne@justincoyne.com>
2023-05-25 16:52:25 -0500

    Extract Searchworks::Holdings to its own class
    In Searchworks the bib data and holdings data come from the same request, but these are separate requests in Folio, so separating the two models now makes it easier to migrate

---
+++ app/models/searchworks/holdings.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module Searchworks
+  ###
+  #  Winnows down the entire holdings to just what was requested by the user
+  ###
+  class Holdings
+    # @param [Request] request the users request
+    # @param [Array<#code>] holdings all of the holdings for the requested item
+    def initialize(request, holdings)
+      @request = request
+      @holdings = holdings
+    end
+
+    # @return [Array<OpenStruct>] a list of every holding in the requested library/location with the given barcodes
+    def where(barcodes: [])
+      return [] if barcodes.empty?
+
+      all.select do |item|
+        barcodes.include?(item.barcode)
+      end
+    end
+
+    # @return [Array<OpenStruct>] a list of every holding in the requested library/location
+    def all
+      return [] if location.blank?
+
+      location.items.map do |item|
+        item.request_status = @request.item_status(item.barcode)
+        item
+      end
+    end
+
+    def single_checked_out_item?
+      all.one? &&
+        all.first.current_location.try(:code) == 'CHECKEDOUT'
+    end
+
+    private
+
+    def library
+      return if @holdings.blank?
+
+      @holdings.find do |library|
+        library.code == @request.origin
+      end
+    end
+
+    def location
+      return if library.blank?
+
+      library.locations.find do |location|
+        location.code == @request.origin_location
+      end
+    end
+  end
+end
---
+++ spec/models/searchworks/holdings_spec.rb
@@ -0,0 +1,140 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Searchworks::Holdings do
+  subject(:requested_holdings) { described_class.new(item.request, item.holdings) }
+
+  describe '#all' do
+    describe 'items that exist' do
+      let(:item) { build(:green_stacks_searchworks_item) }
+
+      it 'are present for the requested location' do
+        expect(subject.all).to be_a Array
+        expect(subject.all.length).to eq 1
+        expect(subject.all.first.barcode).to eq '12345678'
+        expect(subject.all.first.callnumber).to eq 'ABC 123'
+      end
+
+      it 'adds the request_status object to the items' do
+        expect(subject.all.first.request_status).to be_a ItemStatus
+      end
+    end
+
+    describe 'items that do not exist' do
+      let(:item) { build(:green_stacks_searchworks_item) }
+
+      before do
+        allow(item).to receive_messages(request: build(:request, origin: 'SAL3', origin_location: 'STACKS'))
+      end
+
+      it 'are not present for the requested location' do
+        expect(subject.all).to eq([])
+      end
+    end
+  end
+
+  describe '#single_checked_out_item?' do
+    subject(:requested_holdings) do
+      described_class.new(request, holdings)
+    end
+
+    let(:request) { build(:request) }
+    let(:holdings) { JSON.parse(holdings_json, object_class: OpenStruct) }
+
+    context 'when the holdings include a single checked out item' do
+      let(:holdings_json) do
+        <<~JSON
+          [{
+            "code": "BIOLOGY",
+            "locations": [{
+              "code": "STACKS",
+              "items": [{
+                "barcode": "87654321",
+                "callnumber": "ABC 321",
+                "current_location": {
+                  "code": "CHECKEDOUT"
+                },
+                "due_date": "01/01/2015",
+                "type": "STKS",
+                "status": {
+                  "availability_class": "page",
+                  "status_text": "Available"
+                }
+              }]
+            }]
+          }]
+        JSON
+      end
+
+      it { is_expected.to be_single_checked_out_item }
+    end
+
+    context 'when the holdings includes multiple items' do
+      let(:holdings_json) do
+        <<~JSON
+          [{
+            "code": "BIOLOGY",
+            "locations": [{
+              "code": "STACKS",
+              "items": [{
+                "barcode": "12345678",
+                "callnumber": "ABC 123",
+                "type": "STKS",
+                "status": {
+                  "availability_class": "available",
+                  "status_text": "Available"
+                }
+              }, {
+                "barcode": "87654321",
+                "callnumber": "ABC 321",
+                "current_location": {
+                  "code": "CHECKEDOUT"
+                },
+                "due_date": "01/01/2015",
+                "type": "STKS",
+                "status": {
+                  "availability_class": "page",
+                  "status_text": "Available"
+                }
+              }]
+            }]
+          }]
+        JSON
+      end
+
+      it { is_expected.not_to be_single_checked_out_item }
+    end
+  end
+
+  describe '#where' do
+    let(:item) { build(:green_stacks_multi_holdings_searchworks_item) }
+
+    context 'when given an array of barcodes' do
+      subject(:by_barcodes) { requested_holdings.where(barcodes: %w(3610512345678 3610587654321)) }
+
+      it 'returns the items' do
+        expect(by_barcodes).to be_a Array
+        expect(by_barcodes.length).to eq 2
+        expect(by_barcodes.first.barcode).to eq '3610512345678'
+        expect(by_barcodes.last.barcode).to eq '3610587654321'
+      end
+    end
+
+    context 'when given a single barcode' do
+      subject(:by_barcodes) { requested_holdings.where(barcodes: ['12345679']) }
+
+      it 'returns the items' do
+        expect(by_barcodes).to be_a Array
+        expect(by_barcodes.length).to eq 1
+        expect(by_barcodes.first.barcode).to eq '12345679'
+      end
+    end
+
+    context 'when the given barcode does not exist' do
+      subject(:by_barcodes) { requested_holdings.where(barcodes: ['not-a-barcode']) }
+
+      it { is_expected.to be_empty }
+    end
+  end
+end
