f4e6f49201ef3233a3665f649ac8af3d33738787
Justin Coyne <jcoyne@justincoyne.com>
2023-05-26 17:20:21 -0500

    Work on HoldRecallController spec

--- spec/controllers/hold_recall_controller_spec.rb
+++ spec/controllers/hold_recall_controller_spec.rb
@@ -2,13 +2,56 @@
 
 require 'rails_helper'
 
-describe HoldRecallsController do
+RSpec.describe HoldRecallsController do
   let(:hold_recall) { create(:hold_recall) }
   let(:normal_params) do
     { item_id: '1234', barcode: '36105212925395', origin: 'GREEN', origin_location: 'STACKS', destination: 'GREEN' }
   end
 
+
+  let(:folio_holding_response) do
+    { 'instanceId' => 'f1c52ab3-721e-5234-9a00-1023e034e2e8',
+      'source' => 'MARC',
+      'modeOfIssuance' => 'single unit',
+      'natureOfContent' => [],
+      'holdings' => [],
+      'items' =>
+       [{ 'id' => '584baef9-ea2f-5ff5-9947-bbc348aee4a4',
+          'status' => 'Available',
+          'barcode' => '3610512345678',
+          'location' =>
+          { 'effectiveLocation' => { 'code' => 'GRE-STACKS' },
+            'permanentLocation' => { 'code' => 'GRE-STACKS' },
+            'temporaryLocation' => {} },
+          'holdingsRecordId' => 'd1d495e8-7436-540b-a55a-5dfccfba25a3',
+          'permanentLoanType' => 'Can circulate',
+          'suppressFromDiscovery' => false },
+        { 'id' => '99466f50-2b8c-51d4-8890-373190b8f6c4',
+          'status' => 'Available',
+          'barcode' => '12345679',
+          'location' =>
+          { 'effectiveLocation' => { 'code' => 'GRE-STACKS' },
+            'permanentLocation' => { 'code' => 'GRE-STACKS' },
+            'temporaryLocation' => {} },
+          'holdingsRecordId' => 'd1d495e8-7436-540b-a55a-5dfccfba25a3',
+          'permanentLoanType' => 'Can circulate',
+          'suppressFromDiscovery' => false },
+        { 'id' => 'deec4ae9-545c-5d60-85b0-b1048b9dad05',
+          'status' => 'Available',
+          'barcode' => '36105028330483',
+          'location' =>
+          { 'effectiveLocation' => { 'code' => 'GRE-STACKS' },
+            'permanentLocation' => { 'code' => 'GRE-STACKS' },
+            'temporaryLocation' => {} },
+          'holdingsRecordId' => 'd1d495e8-7436-540b-a55a-5dfccfba25a3',
+          'permanentLoanType' => 'Can circulate',
+          'suppressFromDiscovery' => false }] }
+  end
+
   before do
+    allow_any_instance_of(FolioClient).to receive(:find_instance).and_return({ indexTitle: 'Item Title' })
+    allow_any_instance_of(FolioClient).to receive(:resolve_to_instance_id).and_return('f1c52ab3-721e-5234-9a00-1023e034e2e8')
+    allow_any_instance_of(FolioClient).to receive(:items_and_holdings).and_return(folio_holding_response)
     allow(controller).to receive_messages(current_user: user)
   end
 
