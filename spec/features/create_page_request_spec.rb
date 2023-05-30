e1a8a8f5f7c117bf538cd74c7f55cfe6526660bf
Justin Coyne <jcoyne@justincoyne.com>
2023-05-30 14:31:28 -0500

    Get CreatePageRequest spec running

--- spec/features/create_page_request_spec.rb
+++ spec/features/create_page_request_spec.rb
@@ -5,8 +5,50 @@ require 'rails_helper'
 RSpec.describe 'Creating a page request' do
   let(:user) { create(:sso_user) }
 
+  let(:folio_holding_response) do
+    { 'instanceId' => 'f1c52ab3-721e-5234-9a00-1023e034e2e8',
+      'source' => 'MARC',
+      'modeOfIssuance' => 'single unit',
+      'natureOfContent' => [],
+      'holdings' => [],
+      'items' =>
+       [{ 'id' => '584baef9-ea2f-5ff5-9947-bbc348aee4a4',
+          'status' => 'Available',
+          'barcode' => '3610512345678',
+          'location' =>
+          { 'effectiveLocation' => { 'code' => 'GRE-STACKS' },
+            'permanentLocation' => { 'code' => 'GRE-STACKS' },
+            'temporaryLocation' => {} },
+          'callNumber' =>
+            { 'typeId' => '6caca63e-5651-4db6-9247-3205156e9699', 'typeName' => 'Other scheme', 'callNumber' => 'ABC 123' },
+          'holdingsRecordId' => 'd1d495e8-7436-540b-a55a-5dfccfba25a3',
+          'permanentLoanType' => 'Can circulate',
+          'suppressFromDiscovery' => false },
+        { 'id' => '99466f50-2b8c-51d4-8890-373190b8f6c4',
+          'status' => 'Available',
+          'barcode' => '3610587654321',
+          'location' =>
+          { 'effectiveLocation' => { 'code' => 'GRE-STACKS' },
+            'permanentLocation' => { 'code' => 'GRE-STACKS' },
+            'temporaryLocation' => {} },
+          'callNumber' =>
+            { 'typeId' => '6caca63e-5651-4db6-9247-3205156e9699', 'typeName' => 'Other scheme', 'callNumber' => 'ABC 321' },
+          'holdingsRecordId' => 'd1d495e8-7436-540b-a55a-5dfccfba25a3',
+          'permanentLoanType' => 'Can circulate',
+          'suppressFromDiscovery' => false }] }
+  end
+
   before do
     allow(Request.ils_job_class).to receive(:perform_now)
+    allow_any_instance_of(FolioClient).to receive(:find_instance).and_return({ indexTitle: 'Item Title' })
+    allow_any_instance_of(FolioClient).to receive(:resolve_to_instance_id).and_return('f1c52ab3-721e-5234-9a00-1023e034e2e8')
+    allow_any_instance_of(FolioClient).to receive(:items_and_holdings).and_return(folio_holding_response)
+  end
+
+  describe 'item information' do
+    it 'displays the items title' do
+      visit new_page_path(item_id: '2824966', origin: 'GREEN', origin_location: 'STACKS')
+    end
   end
 
   context 'when initiated by an anonmyous user' do
