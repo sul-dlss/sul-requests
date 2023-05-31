1c32eb332f41e8ee1cec2961add5e1dae53a04a7
Justin Coyne <jcoyne@justincoyne.com>
2023-05-31 17:00:44 -0500

    Fix view spec for searchworks item information

--- spec/views/requests/_searchworks_item_information.html.erb_spec.rb
+++ spec/views/requests/_searchworks_item_information.html.erb_spec.rb
@@ -2,14 +2,15 @@
 
 require 'rails_helper'
 
-describe 'requests/_searchworks_item_information.html.erb' do
+RSpec.describe 'requests/_searchworks_item_information.html.erb' do
   before do
     allow(view).to receive_messages(current_request: request)
-    render
   end
 
   describe 'persisted objects' do
     let(:request) { create(:scan, :with_holdings) }
+
+    before { render }
 
     it 'displays the stored item title in an h2' do
       expect(rendered).to have_css('h2', text: 'SAL Item Title')
@@ -18,6 +19,13 @@ describe 'requests/_searchworks_item_information.html.erb' do
 
   describe 'non-persisted scans' do
     let(:request) { Scan.new(item_id: '2824966') }
+
+    before do
+      allow(Settings.ils.bib_model.constantize).to receive(:new)
+        .and_return(double(:bib_data,
+                           title: 'When do you need an antacid? : a burning question'))
+      render
+    end
 
     it 'displays the API fetched item title in an h2', allow_apis: true do
       expect(rendered).to have_css('h2', text: /When do you need an antacid\? : a burning question/)
