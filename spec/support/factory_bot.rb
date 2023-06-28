506a4c4fe16ef24b1bffa60ae1efc171f4b4532a
Justin Coyne <jcoyne@justincoyne.com>
2023-06-28 17:14:47 -0500

    Do not lint factories
    This triggers remote requests

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -7,12 +7,12 @@ RSpec.configure do |config|
   # Run factory_bot linter to validate all fixtures
   config.before(:suite) do
     DatabaseCleaner.start
-    factories_to_lint = FactoryBot.factories.reject do |factory|
-      factory.name == :scan || # Produces an invalid object (no matching holdings)
-        [Hash, SearchworksItem].include?(factory.build_class)  # non-activerecord objects (Hashes)
-    end
+    # factories_to_lint = FactoryBot.factories.reject do |factory|
+    #   factory.name == :scan || # Produces an invalid object (no matching holdings)
+    #     [Hash, SearchworksItem].include?(factory.build_class)  # non-activerecord objects (Hashes)
+    # end
 
-    FactoryBot.lint factories_to_lint unless config.files_to_run.one?
+    # FactoryBot.lint factories_to_lint unless config.files_to_run.one?
   ensure
     DatabaseCleaner.clean
   end
