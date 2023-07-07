fb956109dd607ebf5c4f279286d312ce0279efe5
Chris Beer <chris@cbeer.info>
2023-07-07 15:42:23 -0700

    Finish pushing hold/recallable into the holdings model

--- app/models/request_abilities.rb
+++ app/models/request_abilities.rb
@@ -5,7 +5,6 @@ class RequestAbilities
   def self.rules
     @rules ||= {
       pageable: LocationRules.new(Settings.pageable),
-      hold_recallable: LocationRules.new(Settings.hold_recallable),
       scannable: LocationRules.new(Settings.scannable)
     }
   end
@@ -42,13 +41,11 @@ class RequestAbilities
 
   # returns a true if any of the following is true
   #   - The incoming request includes a barcode (which means an item-level link, likely a checked out item)
-  #   - The origin location is an allowed recallable location by Settings.hold_recallable
   #   - All items in the holdings are hold/recallable, e.g. checked out, on order, in transit, etc.
   def hold_recallable?
     return false unless Settings.features.hold_recall_service
 
     request.barcode_present? ||
-      hold_recallable_location? ||
       all_items_hold_recallable?
   end
 
@@ -68,10 +65,6 @@ class RequestAbilities
 
   def all_items_hold_recallable?
     request.holdings_object.any? && request.holdings_object.all?(&:hold_recallable?)
-  end
-
-  def hold_recallable_location?
-    applicable_rules(:hold_recallable).any?
   end
 
   def applicable_rules(request_type)
