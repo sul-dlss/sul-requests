fa7f71c163bb8d2bd236caed8403e8a144eb2e8d
Justin Coyne <jcoyne@justincoyne.com>
2023-07-28 11:41:27 -0500

    Look for items with a permanent location
    This provides parity with how we find relevant items in Searchworks.
    Fixes #1642

---
+++ spec/models/folio/holdings_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Folio::Holdings do
+  subject(:holdings) { described_class.new(request, items) }
+
+  let(:request) { HoldRecall.new(barcode: '36105237669143', item_id: '14820051', origin: 'MUSIC', origin_location: 'RECORDINGS') }
+  let(:items) { [item] }
+  let(:item) do
+    Folio::Item.new(barcode: '123',
+                    status: 'In process',
+                    type: 'multimedia',
+                    callnumber: 'XX(14820051.1)',
+                    public_note: nil,
+                    effective_location: instance_double(Folio::Location),
+                    permanent_location_code:)
+  end
+
+  describe 'Enumerable' do
+    subject { holdings.to_a }
+
+    context "when the request location doesn't match the effectiveLocation" do
+      let(:permanent_location_code) { 'MUS-RECORDINGS' }
+
+      it { is_expected.to eq items }
+    end
+  end
+end
