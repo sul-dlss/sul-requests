aaa5a3d0904882f59933c0346992cea8712fa115
Chris Beer <chris@cbeer.info>
2023-08-07 14:51:47 -0700

    Fall back on the user's barcode if we can't get the univ id

--- app/jobs/submit_iplc_listener_job.rb
+++ app/jobs/submit_iplc_listener_job.rb
@@ -85,7 +85,7 @@ class SubmitIplcListenerJob < ApplicationJob
     end
 
     def iplc_patron_id
-      request.user.university_id
+      request.user.university_id || request.user.barcode
     end
 
     # Symphony location codes can be converted to ReShare location codes by:
