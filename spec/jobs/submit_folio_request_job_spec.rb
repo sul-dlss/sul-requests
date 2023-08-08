2455668e595d2f235b7d565ce7893193502b0473
Huda Khan <hudakhan@stanford.edu>
2023-08-08 16:42:24 -0600

    creating new folio holding factory instead of updating existing

--- spec/jobs/submit_folio_request_job_spec.rb
+++ spec/jobs/submit_folio_request_job_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe SubmitFolioRequestJob do
     end
 
     context 'with an sso user' do
-      let(:request) { create(:hold_recall_with_holdings, barcodes: ['12345678'], user:) }
+      let(:request) { create(:hold_recall_with_holdings_folio, barcodes: ['12345678'], user:) }
 
       it 'calls the create_item_hold API method' do
         expect { described_class.perform_now(request.id) }.to change { request.folio_command_logs.count }.by(1)
