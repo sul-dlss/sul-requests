5d08c654780ce9b3db7afa810e357e0dff647cd8
Chris Beer <chris@cbeer.info>
2023-09-01 17:28:53 -0700

    Handle data errors with an exception handler

--- app/controllers/circ_check_controller.rb
+++ app/controllers/circ_check_controller.rb
@@ -6,10 +6,18 @@
 class CircCheckController < ApplicationController
   layout false
 
+  rescue_from StandardError, with: :render_error
+
   def index; end
 
   def show
     result = FolioGraphqlClient.new.circ_check(barcode: params[:barcode])
-    @item = result.dig('data', 'items', 0)
+    @item = result&.dig('data', 'items', 0)
+    raise "Barcode does not exist: #{params[:barcode]}" unless @item
+  end
+
+  def render_error(error)
+    Honeybadger.notify(error)
+    render 'error', locals: { error: }
   end
 end
