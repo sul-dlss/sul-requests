4c0315f0cab7ab8dcab482630b39f43672330d36
Chris Beer <chris@cbeer.info>
2024-03-20 16:14:26 -0700

    Update axe test setup now that the controller requires certain parameters

--- spec/features/axe_spec.rb
+++ spec/features/axe_spec.rb
@@ -4,10 +4,14 @@ require 'rails_helper'
 require 'axe-rspec'
 
 RSpec.describe 'Accessibility testing', :js do
+  before do
+    allow(Folio::Instance).to receive(:fetch).with('a12345').and_return(build(:sal3_holding))
+  end
+
   # TODO: once user login is avaliable add user mocks
   # TODO: add patron_request_path after built out more
   it 'validates the home page' do
-    visit new_patron_request_path
+    visit new_patron_request_path(instance_hrid: 'a12345', origin_location_code: 'SAL3')
     expect(page).to be_accessible
   end
 
