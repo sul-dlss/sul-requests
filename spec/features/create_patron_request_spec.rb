3bec921a0af437a294251dacae82043369c71499
Chris Beer <chris@cbeer.info>
2024-04-02 16:31:18 -0700

    Add an integration test for placing a page request

---
+++ spec/features/create_patron_request_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Creating a page request' do
+  let(:user) { create(:sso_user) }
+  let(:bib_data) { build(:single_holding) }
+  let(:patron) do
+    instance_double(Folio::Patron, id: user.patron_key, display_name: 'A User', exists?: true, email: nil,
+                                   patron_group: { desc: 'faculty' },
+                                   ilb_eligible?: true, blocks: ['there is a block'])
+  end
+
+  before do
+    stub_bib_data_json(bib_data)
+  end
+
+  context 'with an SSO user' do
+    let(:current_user) { CurrentUser.new(username: user.sunetid, patron_key: user.patron_key, shibboleth: true) }
+
+    before do
+      allow(Settings.ils.patron_model.constantize).to receive(:find_by).with(patron_key: user.patron_key).and_return(patron)
+      login_as(current_user)
+    end
+
+    it 'submits the request' do
+      visit new_patron_request_path(instance_hrid: 'a1234', origin_location_code: 'SAL3-STACKS')
+      click_on 'Log in with SUNet ID'
+
+      expect { click_on 'Submit' }.to change(PatronRequest, :count).by(1)
+    end
+  end
+end
