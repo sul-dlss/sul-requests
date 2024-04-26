9b11ed09247ec14cbd27b346ce7ba3b6025746fe
Huda Khan <hudakhan@stanford.edu>
2024-04-26 17:21:21 -0600

    changing URL for patron request mailer preview

--- spec/mailers/previews/patron_request_mailer_preview.rb
+++ spec/mailers/previews/patron_request_mailer_preview.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-# Preview all emails at http://localhost:3000/rails/mailers/patron_request
+# Preview all emails at http://localhost:3000/rails/mailers/patron_request_mailer
 class PatronRequestMailerPreview < ActionMailer::Preview
 end
