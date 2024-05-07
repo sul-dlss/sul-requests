ef8547344d30554ed969896e44fc01e500a88c1c
Chris Beer <cabeer@stanford.edu>
2024-05-07 07:51:02 -0700

    Update IlbMailer tests

--- app/mailers/ilb_mailer.rb
+++ app/mailers/ilb_mailer.rb
@@ -4,15 +4,6 @@
 #  Mailer class to send an email to InterLibrary Borrowing (ILB) staff when a request fails to write a transaction to ILLiad
 ###
 class IlbMailer < ApplicationMailer
-  def ilb_notification(request)
-    @request = request
-    mail(
-      to: Settings.illiad_email_address,
-      from: Settings.libraries.default.contact_info.email,
-      subject: 'ILLiad request problem, please remediate'
-    )
-  end
-
   def failed_ilb_notification(patron_request, illiad_response = nil)
     @patron_request = patron_request
     @illiad_response = illiad_response
