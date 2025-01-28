831c291210d827c2c2391354f83c67c1807b8754
Justin Coyne <jcoyne@justincoyne.com>
2025-01-28 14:23:21 -0600

    Remove unnecessary configuration around background jobs
    We always have them enabled

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,9 +70,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  if Settings.background_jobs&.enabled == true
-    config.active_job.queue_adapter = :sidekiq
-  end
+  config.active_job.queue_adapter = :sidekiq
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
