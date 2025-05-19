5a03778ead5208e5a8585f7d830ea2441553ad40
Steve Taylor <sctaylor@stanford.edu>
2025-05-19 08:18:19 -0700

    Remove unused delegate
    `locations` is not defined in FolioGraphqlClient. Multiple definitions
    of locations is causing a linter error.

--- app/services/folio_client.rb
+++ app/services/folio_client.rb
@@ -21,7 +21,7 @@ class FolioClient
 
   attr_reader :base_url
 
-  delegate :locations, :service_points, to: :folio_graphql_client
+  delegate :service_points, to: :folio_graphql_client
 
   def initialize(url: Settings.folio.okapi_url, tenant: Settings.folio.tenant)
     uri = URI.parse(url)
