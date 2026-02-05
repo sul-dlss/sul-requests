a1863c1874ec167926fa1d92e81ad0204db4d555
Chris Beer <cabeer@stanford.edu>
2026-02-05 15:32:23 -0800

    Perform patron request serches using the database.

--- app/services/patron_request_search.rb
+++ app/services/patron_request_search.rb
@@ -13,26 +13,56 @@ class PatronRequestSearch
   attr_reader :params
 
   def call
-    patron_request = search_date_range(PatronRequest, params)
-
-    patron_request = patron_request.where(service_point_code: params['service_point_code']) if params['service_point_code']
-
-    patron_request = filter_results(patron_request, params['request_type'], 'type')
-
-    filter_results(patron_request, params['origin_library_code'], 'origin_library_code')
+    patron_request = PatronRequest
+    patron_request = search_date_range(patron_request)
+    patron_request = search_origin_library(patron_request)
+    patron_request = search_destination_service_point(patron_request)
+    search_request_type(patron_request)
   end
 
-  def search_date_range(search_list, params)
-    return search_list unless params['start_date']
+  def search_date_range(patron_request)
+    return patron_request unless params['start_date']
 
     start_date = Date.parse(params['start_date'])
     end_date = Date.parse(params['end_date'])
-    search_list.where(created_at: start_date.beginning_of_day..end_date.end_of_day)
+    patron_request.where(created_at: start_date.beginning_of_day..end_date.end_of_day)
   end
 
-  def filter_results(search_list, param, field)
-    return search_list unless param
+  def search_origin_library(patron_request)
+    return patron_request unless params['origin_library_code']
 
-    search_list.select { |record| param.include?(record.public_send(field)) }
+    patron_request.where(origin_location_code: params['origin_library_code'].flat_map do |code|
+      Folio::Types.libraries.find_by(code: code)&.locations&.map(&:code)
+    end)
+  end
+
+  def search_destination_service_point(patron_request)
+    return patron_request unless params['service_point_code']
+
+    patron_request.where(service_point_code: params['service_point_code'])
+  end
+
+  def search_request_type(patron_request) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
+    return patron_request unless params['request_type']
+
+    filtered_request = patron_request.where(request_type: params['request_type'].flat_map do |type|
+      case type
+      when 'Mediated page'
+        ['mediated', 'mediated/approved', 'mediated/done']
+      when 'Hold', 'Recall', 'Page'
+        'pickup'
+      else
+        type.downcase
+      end
+    end)
+
+    fulfillment_types = []
+
+    fulfillment_types << nil if params['request_type']&.include?('Page')
+    fulfillment_types << 'hold' if params['request_type']&.include?('Hold')
+    fulfillment_types << 'recall' if params['request_type']&.include?('Recall')
+    filtered_request = filtered_request.where(fulfillment_type: fulfillment_types) if fulfillment_types.length.positive?
+
+    filtered_request
   end
 end
