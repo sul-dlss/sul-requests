46ebe2da6900b6e6cfa7a180bf1c0c8f8a9856e0
Chris Beer <cabeer@stanford.edu>
2026-02-12 07:29:02 -0800

    Create a place for draft requests.

--- app/controllers/aeon_requests_controller.rb
+++ app/controllers/aeon_requests_controller.rb
@@ -4,5 +4,22 @@
 #  Controller for displaying Aeon requests for a user
 ###
 class AeonRequestsController < ApplicationController
-  def index; end
+  helper_method :type_param
+
+  def index
+    @aeon_requests = (current_user&.aeon&.requests || []).select do |request|
+      case type_param
+      when 'submitted'
+        !request.draft?
+      when 'draft'
+        request.draft?
+      end
+    end
+  end
+
+  def type_param
+    (params[:type] || 'submitted').tap do |type|
+      raise ActionController::RoutingError, "Invalid type parameter: #{params[:type]}" unless %w[submitted draft].include?(type)
+    end
+  end
 end
