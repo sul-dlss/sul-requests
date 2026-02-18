27a904ab696fdd6ceb09b63a8b094b63f9bb6445
Chris Beer <cabeer@stanford.edu>
2026-02-18 14:33:44 -0800

    Extract Ead::Request from the controller.

--- app/controllers/archives_requests_controller.rb
+++ app/controllers/archives_requests_controller.rb
@@ -10,13 +10,6 @@
 class ArchivesRequestsController < ApplicationController
   rescue_from EadClient::Error, with: :handle_ead_client_error
 
-  # Maps from the value in EAD to Aeon's valid site codes
-  REPOSITORY_TO_SITE_CODE = {
-    'Department of Special Collections and University Archives' => 'SPECUA',
-    'Archive of Recorded Sound' => 'ARS',
-    'East Asia Library' => 'EASTASIA'
-  }.freeze
-
   def new
     @ead = EadClient.fetch(ead_url_param)
     @ead_url = ead_url_param
@@ -27,57 +20,27 @@ class ArchivesRequestsController < ApplicationController
     # Fetch EAD data to get the actual collection information
     @ead = EadClient.fetch(params[:ead_url])
 
-    username = current_user.email_address
-    volumes = params[:volumes]&.reject(&:blank?)&.map { |json_str| JSON.parse(json_str) }
+    items = params[:volumes]&.reject(&:blank?)&.map { |json_str| JSON.parse(json_str) }
+    @request = Ead::Request.new(user: current_user, ead: @ead, items:, shipping_option: params[:shipping_option])
 
-    # Submit each volume request and collect results
-    results = volumes.map do |volume|
-      submit_volume_request(volume, username)
-      { volume: volume, success: true }
-    rescue StandardError => e
-      { volume: volume, success: false, error: e.message }
-    end
+    results = @request.create_aeon_requests!
 
     # Separate successes and failures
-    failures = results.reject { |r| r[:success] }
-    successes_count = results.count - failures.count
+    successes, failures = results.partition { |r| r[:success] }
 
     # Set appropriate flash message based on results
     if failures.empty?
-      flash[:notice] = "All #{successes_count} request(s) submitted successfully!"
-    elsif successes_count.zero?
+      flash[:notice] = "All #{successes.count} request(s) submitted successfully!"
+    elsif successes.empty?
       flash[:error] = "All requests failed: #{failures.map { |f| "#{f[:volume]} (#{f[:error]})" }.join('; ')}"
     else
-      flash[:warning] = "#{successes_count} succeeded, #{failures.count} failed: #{failures.pluck(:volume).join(', ')}"
+      flash[:warning] = "#{successes.count} succeeded, #{failures.count} failed: #{failures.pluck(:volume).join(', ')}"
     end
 
     redirect_to new_archives_request_path(value: params[:ead_url])
   end
 
   private
-
-  def submit_volume_request(volume, username)
-    aeon_client = AeonClient.new
-
-    aeon_client.submit_archives_request(
-      username: username,
-      title: @ead.title,
-      author: @ead.creator,
-      call_number: "#{@ead.identifier} #{volume['series']}",
-      volume: volume['subseries'],
-      item_url: @ead.collection_permalink,
-      shipping_option: params[:shipping_option],
-      identifier: @ead.identifier,
-      site: map_repository_to_site_code(@ead.repository)
-    )
-  end
-
-  def map_repository_to_site_code(repository)
-    return nil unless repository
-
-    # TODO: Fallback to SPECUA? Other logic?
-    REPOSITORY_TO_SITE_CODE[repository] || 'SPECUA'
-  end
 
   def ead_url_param
     # Handle both 'value' and 'Value' params for compatibility (Aeon currently accepts Value)
---
+++ app/models/ead/request.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module Ead
+  ##
+  # Model for EAD-based requests
+  class Request
+    attr_reader :user, :ead, :items, :shipping_option
+
+    # Maps from the value in EAD to Aeon's valid site codes
+    REPOSITORY_TO_SITE_CODE = {
+      'Department of Special Collections and University Archives' => 'SPECUA',
+      'Archive of Recorded Sound' => 'ARS',
+      'East Asia Library' => 'EASTASIA'
+    }.freeze
+
+    delegate :title, :creator, :call_number, :identifier, :repository, :collection_permalink, to: :ead
+
+    def initialize(user:, ead:, items: [], shipping_option: nil)
+      @user = user
+      @ead = ead
+      @items = items
+      @shipping_option = shipping_option
+    end
+
+    def create_aeon_requests!
+      items.map do |volume|
+        aeon_client.create_request(as_aeon_create_request_data(volume))
+        { volume: volume, success: true }
+      rescue StandardError => e
+        { volume: volume, success: false, error: e.message }
+      end
+    end
+
+    private
+
+    def as_aeon_create_request_data(volume) # rubocop:disable Metrics/MethodLength
+      AeonClient::CreateRequestData.new(
+        call_number: "#{identifier} #{volume['series']}",
+        ead_number: identifier,
+        item_author: creator,
+        item_citation: nil,
+        item_date: nil,
+        item_info1: @ead.collection_permalink,
+        item_info2: nil,
+        item_info3: nil,
+        item_info4: nil,
+        item_info5: nil,
+        item_subtitle: nil,
+        item_title: title,
+        item_volume: volume['subseries'],
+        shipping_option: shipping_option,
+        site: map_repository_to_site_code(repository),
+        special_request: nil,
+        username: user.email_address
+      )
+    end
+
+    def map_repository_to_site_code(repository)
+      return nil unless repository
+
+      # TODO: Fallback to SPECUA? Other logic?
+      REPOSITORY_TO_SITE_CODE[repository] || 'SPECUA'
+    end
+
+    def aeon_client
+      @aeon_client ||= AeonClient.new
+    end
+  end
+end
