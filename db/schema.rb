962bf11b99d85a0265e3a9cb202d108f2f58d4e6
Chris Beer <cabeer@stanford.edu>
2026-02-26 08:54:12 -0800

    Update the db schema.

--- db/schema.rb
+++ db/schema.rb
@@ -12,54 +12,54 @@
 
 ActiveRecord::Schema[8.1].define(version: 2026_02_06_002343) do
   create_table "admin_comments", force: :cascade do |t|
-    t.string "commenter"
     t.string "comment"
-    t.integer "request_id"
+    t.string "commenter"
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.integer "request_id"
     t.string "request_type", default: "Request"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["request_type", "request_id"], name: "index_admin_comments_on_request_type_and_request_id"
   end
 
   create_table "folio_command_logs", force: :cascade do |t|
-    t.string "pickup_location_id", null: false
-    t.string "user_id", null: false
     t.string "barcode", null: false
+    t.datetime "created_at", null: false
+    t.date "expiration_date", null: false
     t.string "item_id", null: false
     t.string "patron_comments"
-    t.date "expiration_date", null: false
+    t.string "pickup_location_id", null: false
     t.integer "request_id", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id", null: false
     t.index ["request_id"], name: "index_folio_command_logs_on_request_id"
   end
 
   create_table "messages", force: :cascade do |t|
-    t.text "text"
-    t.datetime "start_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
     t.datetime "end_at", precision: nil
     t.string "library"
     t.string "request_type"
-    t.datetime "created_at", precision: nil, null: false
+    t.datetime "start_at", precision: nil
+    t.text "text"
     t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "patron_requests", force: :cascade do |t|
-    t.string "patron_id"
-    t.string "patron_email"
-    t.string "display_type"
-    t.string "instance_hrid"
-    t.date "needed_date"
-    t.string "service_point_code"
-    t.text "data", limit: 4294967295
-    t.string "fulfillment_type"
-    t.string "status"
-    t.string "folio_request_id"
-    t.string "origin_location_code"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "data"
+    t.string "display_type"
+    t.string "folio_request_id"
+    t.string "fulfillment_type"
+    t.string "instance_hrid"
     t.string "mediation_status"
+    t.date "needed_date"
+    t.string "origin_location_code"
+    t.string "patron_email"
+    t.string "patron_id"
     t.string "request_type"
+    t.string "service_point_code"
+    t.string "status"
+    t.datetime "updated_at", null: false
     t.index ["display_type"], name: "index_patron_requests_on_display_type"
     t.index ["folio_request_id"], name: "index_patron_requests_on_folio_request_id"
     t.index ["instance_hrid"], name: "index_patron_requests_on_instance_hrid"
@@ -71,38 +71,37 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_06_002343) do
   end
 
   create_table "requests", force: :cascade do |t|
-    t.string "type"
-    t.date "needed_date"
-    t.string "status"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.string "origin"
-    t.string "destination"
-    t.string "origin_location"
-    t.string "item_id"
-    t.integer "user_id"
-    t.text "data"
-    t.text "item_title"
-    t.text "barcodes"
-    t.string "estimated_delivery"
     t.integer "approval_status", default: 0
+    t.text "barcodes"
+    t.datetime "created_at", precision: nil, null: false
+    t.text "data"
+    t.string "destination"
+    t.string "estimated_delivery"
+    t.string "item_id"
+    t.text "item_title"
+    t.date "needed_date"
+    t.string "origin"
+    t.string "origin_location"
+    t.string "status"
+    t.string "type"
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "user_id"
     t.boolean "via_borrow_direct", default: false
     t.index ["needed_date"], name: "index_requests_on_needed_date"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "sunetid"
-    t.string "name"
-    t.string "email"
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.string "email"
     t.string "library_id"
+    t.string "name"
     t.string "student_type"
+    t.string "sunetid"
     t.string "univ_id"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["library_id"], name: "index_users_on_library_id"
     t.index ["sunetid"], name: "unique_users_by_sunetid", unique: true
   end
-
 end
