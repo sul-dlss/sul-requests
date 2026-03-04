8c40eed50278871f275e0453c1e12aee928047c9
Chris Beer <cabeer@stanford.edu>
2026-03-04 12:35:34 -0800

    Rename ead_request ivar and form data to patron_request

--- app/controllers/archives_requests_controller.rb
+++ app/controllers/archives_requests_controller.rb
@@ -5,7 +5,7 @@
 class ArchivesRequestsController < ApplicationController
   include AeonController
 
-  before_action :load_ead_request, only: [:new, :create]
+  before_action :load_patron_request, only: [:new, :create]
   before_action :authorize_new_request, only: [:new]
   rescue_from EadClient::Error, with: :handle_ead_client_error
 
@@ -22,11 +22,11 @@ class ArchivesRequestsController < ApplicationController
   def create
     authorize! :create, Aeon::Request
 
-    @ead_request.save!
+    @patron_request.save!
 
-    @ead_request.submit_later
+    @patron_request.submit_later
 
-    redirect_to archives_request_path(@ead_request.id)
+    redirect_to archives_request_path(@patron_request.id)
   end
 
   private
@@ -38,23 +38,23 @@ class ArchivesRequestsController < ApplicationController
   end
 
   def new_params
-    return {} unless params[:ead_request]
+    return {} unless params[:patron_request]
 
-    item_keys = params.dig(:ead_request, :items)&.keys || []
+    item_keys = params.dig(:patron_request, :items)&.keys || []
     item_values = [:series, :for_publication, :subseries, :requested_pages, :additional_information, :appointment_id]
 
-    params.expect(ead_request: [:ead_url, :request_type, { volumes: [], items: item_keys.index_with { item_values } }]).tap do |p|
+    params.expect(patron_request: [:ead_url, :request_type, { volumes: [], items: item_keys.index_with { item_values } }]).tap do |p|
       p['items'] = p['items'].values if p['items'].respond_to?(:values)
     end
   end
 
-  def load_ead_request
-    @ead_request = PatronRequest.new(user: current_user, ead_url: ead_url_param,
-                                     **new_params.to_h.transform_keys({ items: :aeon_item, volumes: :barcodes }.with_indifferent_access))
+  def load_patron_request
+    @patron_request = PatronRequest.new(user: current_user, ead_url: ead_url_param,
+                                        **new_params.to_h.transform_keys({ items: :aeon_item, volumes: :barcodes }.with_indifferent_access))
   end
 
   def ead_url_param
-    return params.dig(:ead_request, :ead_url) if params[:ead_request]
+    return params.dig(:patron_request, :ead_url) if params[:patron_request]
     # Handle both 'value' and 'Value' params for compatibility (Aeon currently accepts Value)
     return params.require(:Value) if params[:Value].present?
 
