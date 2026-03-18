52e410ccb054671ae0fe503962540eb07c5b5e91
Huda Khan <hudakhan@stanford.edu>
2026-03-17 18:19:46 -0600

    updating tests

--- spec/features/delete_draft_requests_spec.rb
+++ spec/features/delete_draft_requests_spec.rb
@@ -14,6 +14,10 @@ RSpec.describe 'Bulk delete actions and modal', :js do
     build(:aeon_request, call_number: 'PR9195.1 .S56 NO.2', title: 'Slow poetry in America : a poetry quarterly',
                          transaction_number: 101, username: aeon_user.username)
   end
+  let(:third_request) do
+    build(:aeon_request, call_number: 'PR8195.1 .S56 NO.2', title: 'Fast poetry in America : a poetry monthly',
+                         transaction_number: 102, username: aeon_user.username)
+  end
   let(:draft_queue) do
     Aeon::Queue.new(id: 5, queue_name: 'Awaiting User Review', queue_type: 'Transaction')
   end
@@ -22,21 +26,21 @@ RSpec.describe 'Bulk delete actions and modal', :js do
                     find_user: aeon_user,
                     find_queue: draft_queue,
                     appointments_for: [],
-                    requests_for: [first_request, second_request],
+                    requests_for: [first_request, second_request, third_request],
                     reading_rooms: [],
                     available_appointments: [])
   end
 
   before do
     allow(AeonClient).to receive(:new).and_return(stub_aeon_client)
-    allow(aeon_user).to receive_messages(requests: [first_request, second_request])
+    allow(aeon_user).to receive_messages(requests: [first_request, second_request, third_request])
     login_as(current_user)
     visit draft_aeon_requests_path
   end
 
   describe 'on page load' do
     it 'display delete checkboxes next to each request and a delete all input' do
-      expect(page).to have_css('input[data-draft-request-target="select"]', count: 2)
+      expect(page).to have_css('input[data-draft-request-target="select"]', count: 3)
       expect(page).to have_css('input[data-draft-request-target="selectall"]', count: 1)
     end
   end
@@ -54,15 +58,28 @@ RSpec.describe 'Bulk delete actions and modal', :js do
     end
   end
 
-  describe 'when selecting all requests' do
-    it 'shows a summary description in the modal when all items are selected' do
-      check('select-all-delete')
-      expect(page).to have_css('input[data-draft-request-target="select"]:checked', count: 2)
-      expect(page).to have_button('delete-all')
+  describe 'when selecting two requests' do
+    it 'shows descriptions grouped by type and title' do
+      check('delete-bulk-100')
+      check('delete-bulk-101')
 
       click_button('delete-all')
       expect(page).to have_css('.modal-title', text: 'Delete 2 draft requests?')
-      expect(page).to have_css('.modal-body #request-content', text: '0 digitization and 2 reading room use requests')
+      expect(page).to have_css('.modal-body #request-content', text: 'Reading room use', count: 1)
+      expect(page).to have_css('.modal-body #request-content', text: 'Slow poetry in America : a poetry quarterly', count: 1)
+      expect(page.find('#request-content .flex-row')).to have_content('PR9195.1 .S56 NO.1').and have_content('PR9195.1 .S56 NO.2')
+    end
+  end
+
+  describe 'when selecting all requests' do
+    it 'shows a summary description in the modal when all items are selected' do
+      check('select-all-delete')
+      expect(page).to have_css('input[data-draft-request-target="select"]:checked', count: 3)
+      expect(page).to have_button('delete-all')
+
+      click_button('delete-all')
+      expect(page).to have_css('.modal-title', text: 'Delete 3 draft requests?')
+      expect(page).to have_css('.modal-body #request-content', text: '0 digitization and 3 reading room use requests')
     end
   end
 end
