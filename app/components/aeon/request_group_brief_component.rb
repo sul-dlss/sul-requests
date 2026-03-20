eec1fcf2996ae8e37026066e1c7fa31f7dd743c0
Steve Taylor <sctaylor@stanford.edu>
2026-03-20 09:30:04 -0700

    Split out a specific brief modal component

--- app/components/aeon/request_group_brief_component.rb
+++ app/components/aeon/request_group_brief_component.rb
@@ -4,15 +4,5 @@ module Aeon
   # Render a group of requests that share the same title and request type
   class RequestGroupBriefComponent < Aeon::RequestGroupComponent
     with_collection_parameter :request_group
-
-    def initialize(request_group:, heading_level: :h2)
-      super(request_group:)
-      @heading_level = heading_level
-    end
-
-    def title_tag
-      style_class = @heading_level == :h3 ? 'h4' : 'h3'
-      content_tag(@heading_level, title, class: style_class)
-    end
   end
 end
