aa515d075500e1a341919254b5c2e71228a451e0
Steve Taylor <sctaylor@stanford.edu>
2026-03-20 17:02:48 -0700

    Rename RequestGroupBriefModalComponent and add tests

--- app/components/aeon/request_group_brief_modal_component.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Aeon
-  # Render a group of requests that share the same title and request type for modal display
-  class RequestGroupBriefModalComponent < RequestGroupBriefComponent
-  end
-end
---
+++ app/components/aeon/request_group_compact_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Aeon
+  # Render a compact view of a group of requests that share the same title and request type
+  class RequestGroupCompactComponent < RequestGroupBriefComponent
+  end
+end
