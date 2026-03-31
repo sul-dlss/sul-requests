143c840e279570dbc4e499340ba4c16a1a66bcce
Chris Beer <cabeer@stanford.edu>
2026-03-31 14:14:41 -0700

    Set the mailer host from settings.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,6 +4,6 @@
 #  Top-level mailer class to allow us to set defaults like from and layout.
 ###
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@requests.stanford.edu'
+  default from: "no-reply@#{Settings.mailer_host}"
   layout 'mailer'
 end
