45d6849f186668ffafcca9486c46ec8c566b9598
dnoneill <noneill@stanford.edu>
2026-04-08 11:15:08 -0400

    css only view

--- app/components/aeon/request_group_brief_component.rb
+++ app/components/aeon/request_group_brief_component.rb
@@ -4,5 +4,10 @@ module Aeon
   # Render a group of requests that share the same title and request type
   class RequestGroupBriefComponent < Aeon::RequestGroupComponent
     with_collection_parameter :request_group
+
+    def initialize(classes: 'request-group border rounded p-3 mb-3', **)
+      super(**)
+      @classes = classes
+    end
   end
 end
