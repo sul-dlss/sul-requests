cf1c9d180a779668abd6fbff689a2c6eacd6a960
dnoneill <noneill@stanford.edu>
2026-04-17 16:56:23 -0400

    implement add items functionality to modal

--- app/components/aeon/request_group_brief_component.rb
+++ app/components/aeon/request_group_brief_component.rb
@@ -4,5 +4,15 @@ module Aeon
   # Render a group of requests that share the same title and request type
   class RequestGroupBriefComponent < Aeon::RequestGroupComponent
     with_collection_parameter :request_group
+
+    def initialize(after_request_item_component: nil, data: {}, **)
+      @after_request_item_component = after_request_item_component
+      @data = data
+      super(**)
+    end
+
+    def data
+      { controller: 'empty-remove', title: }.merge(@data)
+    end
   end
 end
---
+++ app/javascript/controllers/add_items_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  schedule(e) {
+    const appointmentRequests = document.querySelector(this.element.dataset.appointmentTarget);
+    const transactionNumber = e.currentTarget.dataset.transactionNumber;
+    const inAppointments = appointmentRequests.querySelector(`[data-title="${e.currentTarget.dataset.title}"]`)
+    if (!inAppointments){
+      let element = this.element.cloneNode(true);
+      element.querySelectorAll('li').forEach((li) => {
+        if (li.dataset.transactionNumber != transactionNumber) {
+          li.remove()
+        }
+        else { 
+          this.updateButtonForm(li, transactionNumber);
+          this.element.querySelector(`#${li.id}`).remove()
+        }
+      })
+      appointmentRequests.appendChild(element)
+    } else {
+      const addTo = this.element.querySelector(`li[data-transaction-number="${transactionNumber}"]`);
+      this.updateButtonForm(addTo, transactionNumber);
+      inAppointments.querySelector('ul').appendChild(addTo)
+    }
+  }
+  updateButtonForm(element, transactionNumber) {
+    let input = document.createElement("input");
+    input.type = 'hidden'
+    input.name = 'update_requests[]'
+    input.value = transactionNumber
+    element.appendChild(input)
+    element.querySelector('button').remove();
+  }
+}
