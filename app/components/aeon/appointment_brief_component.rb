525a56360134ea3e4bddde02d4c38d8e40ed4a2d
dnoneill <noneill@stanford.edu>
2026-04-22 19:52:01 -0400

    update item limit display

--- app/components/aeon/appointment_brief_component.rb
+++ app/components/aeon/appointment_brief_component.rb
@@ -4,19 +4,5 @@ module Aeon
   # Render aeon apointment card
   class AppointmentBriefComponent < Aeon::AppointmentComponent
     with_collection_parameter :appointment
-
-    def total_item_limit_percentage
-      appointment.requests.count / appointment.reading_room.appointment_item_limit
-    end
-
-    def badge_classes
-      if total_item_limit_percentage >= 1
-        'bg-danger-subtle'
-      elsif total_item_limit_percentage >= 0.8
-        'bg-warning-subtle'
-      else
-        'bg-success-subtle text-dark'
-      end
-    end
   end
 end
