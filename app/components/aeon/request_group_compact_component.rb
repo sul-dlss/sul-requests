84ed50a705b62b0f5b402aaf8c6e814ddea6e12d
Chris Beer <cabeer@stanford.edu>
2026-04-29 10:33:57 -0700

    Untangle RequestGroupBriefComponent + RequestGroupCompactComponent

--- app/components/aeon/request_group_compact_component.rb
+++ app/components/aeon/request_group_compact_component.rb
@@ -2,6 +2,7 @@
 
 module Aeon
   # Render a compact view of a group of requests that share the same title and request type
-  class RequestGroupCompactComponent < RequestGroupBriefComponent
+  class RequestGroupCompactComponent < Aeon::RequestGroupComponent
+    with_collection_parameter :request_group
   end
 end
