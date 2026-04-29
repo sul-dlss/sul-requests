0a4c8ad1d9f0ad30c83e690aede7e652247f8513
Chris Beer <cabeer@stanford.edu>
2026-04-28 17:01:32 -0700

    Extract Aeon::AppointmentDateTimeComponent

--- app/components/aeon/appointment_component.rb
+++ app/components/aeon/appointment_component.rb
@@ -9,27 +9,6 @@ module Aeon
       @appointment = appointment
     end
 
-    def time_range_html(margin: 'me-2')
-      tag.span do
-        if appointment.reading_room.day_only_appointments?
-          tag.i(class: "bi bi-clock #{margin}") + "Open hours: #{appointment_time_range}"
-        else
-          tag.i(class: "bi bi-clock #{margin}") + appointment_time_range
-        end
-      end
-    end
-
-    def appointment_time_range
-      start = l(appointment.start_time, format: :time_only)
-      stop = l(appointment.stop_time, format: :time_only)
-
-      "#{start} - #{stop}"
-    end
-
-    def appointment_date
-      l(appointment.start_time, format: :date_only)
-    end
-
     def add_item_disabled?
       return true unless appointment.editable?
       return false unless appointment.reading_room.appointment_item_limit
