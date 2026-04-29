86b7c1ef42cf616883f248b295da571747f32788
Chris Beer <cabeer@stanford.edu>
2026-04-29 13:10:02 -0700

    Add flags to RequestGroupItemComponent so we can remove RequestGroupPerAppointmentItemComponent

--- app/components/aeon/request_group_item_component.rb
+++ app/components/aeon/request_group_item_component.rb
@@ -9,8 +9,23 @@ module Aeon
 
     delegate :transaction_number, :transaction_date, to: :request
 
-    def initialize(request:)
+    def initialize(request:, actions: true, appointment: true, footer: true)
       @request = request
+      @actions = actions
+      @appointment = appointment
+      @footer = footer
+    end
+
+    def actions?
+      @actions
+    end
+
+    def appointment?
+      @appointment
+    end
+
+    def footer?
+      @footer
     end
   end
 end
