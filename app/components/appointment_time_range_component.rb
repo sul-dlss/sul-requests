b3de352f0db16011c0af36fb098683d5dba02e59
dnoneill <noneill@stanford.edu>
2026-05-01 18:13:20 -0400

    move activities dropdown to list

--- app/components/appointment_time_range_component.rb
+++ app/components/appointment_time_range_component.rb
@@ -2,8 +2,10 @@
 
 # dropdown, confirmation screen appointment display
 class AppointmentTimeRangeComponent < ViewComponent::Base
-  def initialize(appointment:)
+  def initialize(appointment:, show_hours: false, location: nil)
     @appointment = appointment
+    @show_hours = show_hours
+    @location = location
   end
 
   def date
@@ -14,9 +16,15 @@ class AppointmentTimeRangeComponent < ViewComponent::Base
     "#{l(@appointment.start_time, format: :time_only)} - #{l(@appointment.stop_time, format: :time_only)}"
   end
 
-  def call
-    return tag.span(date) if @appointment.reading_room.day_only_appointments?
+  def show_hours?
+    @show_hours || !@appointment.reading_room.day_only_appointments?
+  end
 
-    tag.span(date) + tag.i(class: 'bi bi-dot') + tag.span(time_range)
+  def call
+    values = [tag.span(date)]
+    values << tag.span(time_range) if show_hours?
+    values << tag.span(@location) if @location
+
+    safe_join(values, tag.i(class: 'bi bi-dot'))
   end
 end
