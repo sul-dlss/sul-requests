fd0b0bb837385c82c0fd3ac162a94aa9bb839a44
Huda Khan <hudakhan@stanford.edu>
2026-05-11 14:53:52 -0600

    adding tests, and ensuring field acts properly

--- spec/components/aeon/appointment_selection_component_spec.rb
+++ spec/components/aeon/appointment_selection_component_spec.rb
@@ -3,15 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe Aeon::AppointmentSelectionComponent, type: :component do
-  context 'with existing appointment' do
+  let(:reading_rooms) { JSON.load_file('spec/fixtures/reading_rooms.json') }
+
+  context 'with existing appointment and day only appointment reading room' do
     let(:appointment) { build(:aeon_appointment) }
 
     before do
       render_inline(described_class.new(appointment:))
     end
 
-    it 'renders existing appointment time and date and placeholder for date and time to be selected' do
-      within('.selected-appointment ') do
+    it 'renders existing appointment time and date and placeholder for date to be selected' do
+      within('.selected-appointment') do
+        expect(page).to have_css('h4', text: 'Current')
+        expect(page).to have_text('Mar 11, 2024')
+        expect(page).to have_no_text('1:00 PM -  1:15 PM')
+        expect(page).to have_css('h4', text: 'New')
+        expect(page).to have_css('div[data-appointment-target="selection"]', text: 'Select date')
+      end
+    end
+  end
+
+  context 'with existing appointment and reading room with date and time' do
+    let(:appointment) { build(:aeon_appointment) }
+
+    before do
+      appointment.reading_room = Aeon::ReadingRoom.from_dynamic(reading_rooms[2])
+      render_inline(described_class.new(appointment:))
+    end
+
+    it 'renders existing appointment time and date and placeholder for date to be selected' do
+      within('.selected-appointment') do
         expect(page).to have_css('h4', text: 'Current')
         expect(page).to have_text('Mar 11, 2024')
         expect(page).to have_text('1:00 PM -  1:15 PM')
