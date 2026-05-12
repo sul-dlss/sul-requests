bca9ba9c6569156af5dcf484fe4dc0ea5b873f3c
Huda Khan <hudakhan@stanford.edu>
2026-05-12 16:03:42 -0600

    updating tests

--- spec/components/aeon/appointment_selection_component_spec.rb
+++ spec/components/aeon/appointment_selection_component_spec.rb
@@ -42,15 +42,33 @@ RSpec.describe Aeon::AppointmentSelectionComponent, type: :component do
     end
   end
 
-  context 'with new appointment' do
+  context 'with new appointment for reading room with date and time' do
+    let(:appointment) { Aeon::Appointment.new }
+
     before do
-      render_inline(described_class.new(appointment: Aeon::Appointment.new))
+      appointment.reading_room = Aeon::ReadingRoom.from_dynamic(reading_rooms[2])
+      render_inline(described_class.new(appointment:))
     end
 
     it 'renders only the selection placeholder to be hidden' do
       expect(page).to have_no_text('Current')
       expect(page).to have_no_text('New')
-      expect(page).to have_css('div[data-appointment-target="selection"].d-none')
+      expect(page).to have_css('div[data-appointment-target="selection"]', text: 'Select date and time')
+    end
+  end
+
+  context 'with new appointment for reading room that is day only' do
+    let(:appointment) { Aeon::Appointment.new }
+
+    before do
+      appointment.reading_room = Aeon::ReadingRoom.from_dynamic(reading_rooms[3])
+      render_inline(described_class.new(appointment:))
+    end
+
+    it 'renders only the selection placeholder to be hidden' do
+      expect(page).to have_no_text('Current')
+      expect(page).to have_no_text('New')
+      expect(page).to have_no_css('div[data-appointment-target="selection"]')
     end
   end
 end
