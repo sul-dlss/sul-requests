c50149ff1e9eb4d4833931d8b7340dd215df36b6
Chris Beer <cabeer@stanford.edu>
2026-05-14 08:47:59 -0700

    Pull Checkout#non_renewable_reason over to the component

--- app/components/checkout_component.rb
+++ app/components/checkout_component.rb
@@ -6,11 +6,33 @@ class CheckoutComponent < ViewComponent::Base
 
   delegate :sul_icon, :today_with_time_or_date, :detail_link_to_searchworks, to: :helpers
 
+  delegate :renewable?, :lost?, :recalled?, :renewal_blocked_by_hold?, :claimed_returned?, :unseen_renewals_remaining, :renewal_count,
+           :reserve_item?, :too_soon_to_renew?, :item_category_non_renewable?, to: :checkout, private: true
+
   def initialize(checkout:, patron:)
     @checkout = checkout
     @patron = patron
     super()
   end
+
+  # rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+  def non_renewable_reason
+    return 'Item is assumed lost; you must pay the fee or return the item.' if lost?
+    return 'Another user is waiting for this item.' if recalled? || renewal_blocked_by_hold?
+    return 'Claim review is in process.' if claimed_returned?
+
+    unless unseen_renewals_remaining.positive?
+      return 'No online renewals left; you may renew this item in person.' if renewal_count.positive?
+
+      return 'No online renewals for this item.'
+    end
+
+    return 'Renew Reserve items in person.' if reserve_item?
+    return 'Another user is waiting for this item.' if item_category_non_renewable?
+
+    'Too soon to renew.' if too_soon_to_renew?
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
 
   # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   def render_checkout_status
