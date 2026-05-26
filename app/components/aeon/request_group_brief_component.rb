632a125040cb98923357bc28be0d3abb0b18004f
Steve Taylor <sctaylor@stanford.edu>
2026-05-26 14:46:30 -0700

    Apply the "Add items" styles from the designs

--- app/components/aeon/request_group_brief_component.rb
+++ app/components/aeon/request_group_brief_component.rb
@@ -5,8 +5,11 @@ module Aeon
   class RequestGroupBriefComponent < Aeon::RequestGroupComponent
     with_collection_parameter :request_group
 
-    def initialize(after_request_item_component: nil, data: {}, **)
+    attr_reader :classes
+
+    def initialize(after_request_item_component: nil, classes: %w[request-group border rounded p-3], data: {}, **)
       @after_request_item_component = after_request_item_component
+      @classes = Array(classes)
       @data = data
       super(**)
     end
