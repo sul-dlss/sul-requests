1e7ffe58733577c678d76db3fbbc3959fd559f0d
Chris Beer <cabeer@stanford.edu>
2026-06-11 08:08:45 -0700

    Seed the dev + test databases with basic aeon information.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless Rails.env.production?
+  StubAeonClient::ReadingRoom.destroy_all
+  StubAeonClient::Queue.destroy_all
+
+  JSON.parse(File.read(Rails.root.join('spec', 'fixtures', 'reading_rooms.json'))).each do |reading_room_data|
+    StubAeonClient::ReadingRoom.create!(id: reading_room_data['id'], data: reading_room_data.except('id'), closures: [])
+  end
+
+  JSON.parse(File.read(Rails.root.join('spec', 'fixtures', 'queues.json'))).each do |queue_data|
+    StubAeonClient::Queue.create!(id: queue_data.dig('queue', 'id'), data: queue_data['queue'].except('id'))
+  end
+end
