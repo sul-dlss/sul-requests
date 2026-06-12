b00dbc234f4103824ac1ccbce5148d243e34a0cf
Chris Beer <cabeer@stanford.edu>
2026-06-12 11:28:49 -0700

    Convert searchworks aeon request form to use the stubbed Aeon implementation.

--- spec/factories/stub_aeon.rb
+++ spec/factories/stub_aeon.rb
@@ -6,4 +6,15 @@ FactoryBot.define do
     startTime { Time.zone.parse('2024-03-11T20:00:00Z') }
     stopTime { Time.zone.parse('2024-03-11T20:15:00Z') }
   end
+
+  factory :remote_aeon_activity, class: 'StubAeonClient::Activity' do
+    name { 'An Aeon Activity' }
+    activityType { 'Class visit' }
+    beginDate { Time.zone.parse('2026-02-19T12:00:00') }
+    endDate { Time.zone.parse('2026-02-19T13:00:00') }
+    active { true }
+    activityStatus { 'Pending' }
+    location { 'Special Collections' }
+    users { [] }
+  end
 end
