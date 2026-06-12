db52da5d6588658fb6c02d58c745b5e33ab142aa
Chris Beer <cabeer@stanford.edu>
2026-06-12 14:33:13 -0700

    Pull data loading up to the controller (instead of buried in a component)

--- app/components/aeon/appointment_component.rb
+++ app/components/aeon/appointment_component.rb
@@ -3,13 +3,14 @@
 module Aeon
   # Render aeon apointment card
   class AppointmentComponent < ViewComponent::Base
-    attr_reader :appointment
+    attr_reader :appointment, :all_requests
 
     delegate :current_user, to: :helpers
     delegate :reading_room_id, to: :appointment
 
-    def initialize(appointment:)
+    def initialize(appointment:, all_requests: nil)
       @appointment = appointment
+      @all_requests = all_requests
     end
 
     def add_item_allowed?
@@ -19,7 +20,9 @@ module Aeon
     end
 
     def eligible_saved_for_later_requests
-      @eligible_saved_for_later_requests ||= current_user.aeon.requests.saved_for_later.for_reading_room(@appointment.reading_room)
+      return [] unless all_requests
+
+      @eligible_saved_for_later_requests ||= all_requests.saved_for_later.for_reading_room(@appointment.reading_room)
     end
 
     def items_path
