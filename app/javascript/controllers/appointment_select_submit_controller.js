555cf3a22609f6f534fc4409c02623cf95788dbd
Chris Beer <cabeer@stanford.edu>
2026-06-22 16:50:07 -0700

    Add a spinner when adding an appointment to a request

--- app/javascript/controllers/appointment_select_submit_controller.js
+++ app/javascript/controllers/appointment_select_submit_controller.js
@@ -1,7 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["button"]
+
+  handleSubmit(event) {
+    this.disableForm();
+    this.showPlaceholder();
+  }
+
   disableForm() {
-    this.element.querySelector('button').disabled = true;
+    this.buttonTarget.classList.add("d-none")
+  }
+
+  showPlaceholder() {
+    const placeholder = document.createElement("div");
+    placeholder.classList.add("d-flex", "align-items-center");
+    placeholder.innerHTML = `
+      <span class="spinner-border spinner-border-sm me-2" aria-hidden="true"></span>
+      <span>Saving appointment...</span>
+    `;
+    this.element.appendChild(placeholder);
   }
 }
