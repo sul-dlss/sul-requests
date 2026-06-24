7b99e91b120ade4ec1ef453c4a02024889695f7e
Chris Beer <cabeer@stanford.edu>
2026-06-24 09:28:53 -0700

    Use component slots for icons + status information.

--- app/components/home/summary_card_component.rb
+++ app/components/home/summary_card_component.rb
@@ -3,12 +3,26 @@
 module Home
   # Generic count-with-link card for the card-view home page.
   class SummaryCardComponent < ViewComponent::Base
-    attr_reader :id, :title, :icon, :label, :count, :path, :status,
-                :empty_path, :empty_link_label, :next_up_date, :secondary_label
+    renders_one :icon, ->(icon:) { render_icon(icon) }
+    renders_one :status, types: {
+      next_up: lambda { |date:|
+        if date
+          tag.span(class: 'text-digital-red-dark') do
+            tag.span('Next up: ') + tag.span(date.strftime('%b %-d, %Y'), class: 'fw-semibold')
+          end
+        end
+      },
+      count: lambda { |label:|
+        tag.span(label, class: 'text-digital-red-dark') if label.present?
+      },
+      text: ->(text:) { text }
+    }
+
+    attr_reader :id, :title, :label, :count, :path,
+                :empty_path, :empty_link_label, :secondary_label
 
     # rubocop:disable Metrics/ParameterLists
-    def initialize(id:, title:, icon:, path:, count:, label: nil, secondary_label: nil, status: nil, empty_path: nil, empty_link_label: nil,
-                   next_up_date: nil)
+    def initialize(id:, title:, icon:, path:, count:, label: nil, secondary_label: nil, status: nil, empty_path: nil, empty_link_label: nil)
       # rubocop:enable Metrics/ParameterLists
       @id = id
       @title = title
@@ -20,11 +34,22 @@ module Home
       @status = status
       @empty_path = empty_path
       @empty_link_label = empty_link_label
-      @next_up_date = next_up_date
     end
 
     def empty?
       count.to_i.zero?
+    end
+
+    def default_status
+      @status
+    end
+
+    def default_icon
+      render_icon(@icon)
+    end
+
+    def render_icon(icon)
+      tag.i(class: "bi #{icon} text-60-black fs-4")
     end
   end
 end
