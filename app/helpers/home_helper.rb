9f2b02f9e4d5b8de6794253b9e30763047cf833b
Chris Beer <cabeer@stanford.edu>
2026-06-24 09:28:53 -0700

    Use component slots for icons + status information.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,9 +2,4 @@
 
 # Helpers for the home page redesign views.
 module HomeHelper
-  def count_callout(label:)
-    return if label.blank?
-
-    tag.span(label, class: 'text-digital-red-dark')
-  end
 end
