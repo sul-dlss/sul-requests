62b37a165113b5b2021c7f5f83e819594b23258b
Chris Beer <cabeer@stanford.edu>
2026-06-25 07:43:10 -0700

    Use slots for home list view cards.

--- app/components/home/list_view/summary_card_component.rb
+++ app/components/home/list_view/summary_card_component.rb
@@ -4,18 +4,28 @@ module Home
   module ListView
     # Generic count-with-teaser card for the list-view home page.
     class SummaryCardComponent < ViewComponent::Base
+      renders_one :icon, ->(icon:) { render_icon(icon) }
+      renders_one :status, ->(&block) { tag.p(class: 'text-digital-red-dark', &block) }
+
       renders_one :items
 
-      attr_reader :title, :icon, :count, :pill_noun, :view_all_label, :view_all_path, :status
+      attr_reader :title, :count, :pill_noun, :view_all_label, :view_all_path
 
-      def initialize(title:, icon:, count:, view_all_label:, view_all_path:, pill_noun: 'total', status: nil) # rubocop:disable Metrics/ParameterLists
+      def initialize(title:, icon_class:, count:, view_all_label:, view_all_path:, pill_noun: 'total') # rubocop:disable Metrics/ParameterLists
         @title = title
-        @icon = icon
+        @icon_class = icon_class
         @count = count
         @pill_noun = pill_noun
         @view_all_label = view_all_label
         @view_all_path = view_all_path
-        @status = status
+      end
+
+      def default_icon
+        render_icon(@icon_class)
+      end
+
+      def render_icon(icon)
+        tag.i(class: "bi #{icon} text-60-black fs-4 me-3")
       end
 
       def render? = count.positive?
