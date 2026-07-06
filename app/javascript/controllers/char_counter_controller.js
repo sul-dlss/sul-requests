e756c399144b7dea52e98bce090253ea34ca8af0
Chris Beer <cabeer@stanford.edu>
2026-07-06 09:03:34 -0700

    Extract classes attribute for CharCounterController

--- app/javascript/controllers/char_counter_controller.js
+++ app/javascript/controllers/char_counter_controller.js
@@ -3,12 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // For an input form element put 
 export default class extends Controller {
   static targets = ["counter"]
+  static classes = ["default", "warning"]
+  static values = {
+    warningThreshold: { type: Number, default: 10 }
+  }
 
   updateCharCounter(event) {
     const currentChars = event.currentTarget.value.length;
     const maxChars = event.currentTarget.maxLength;
-    const charClass = maxChars - currentChars < 10 ? 'text-cardinal' : 'text-body-tertiary';
+
     this.counterTarget.innerHTML = `${currentChars}/${maxChars}`;
-    this.counterTarget.classList = `fs-14 ${charClass}`;
+
+    if (maxChars - currentChars < this.warningThresholdValue) {
+      this.counterTarget.classList.add(this.warningClass);
+      this.counterTarget.classList.remove(this.defaultClass);
+    } else {
+      this.counterTarget.classList.remove(this.warningClass);
+      this.counterTarget.classList.add(this.defaultClass);
+    }
   }
 }
