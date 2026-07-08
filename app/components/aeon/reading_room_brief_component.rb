528e7cc5c46ec6e74dcd1c735d1cba771a955a79
Chris Beer <cabeer@stanford.edu>
2026-07-08 07:42:06 -0700

    Add create_appointment attribute to ReadingRoomBriefComponent so we render the correct thing

--- app/components/aeon/reading_room_brief_component.rb
+++ app/components/aeon/reading_room_brief_component.rb
@@ -3,8 +3,13 @@
 module Aeon
   # Render an accordion item for a digitization form step.
   class ReadingRoomBriefComponent < ViewComponent::Base
-    def initialize(reading_room:)
+    def initialize(reading_room:, create_appointment: false)
       @reading_room = reading_room
+      @create_appointment = create_appointment
+    end
+
+    def create_appointment?
+      @create_appointment
     end
 
     attr_reader :reading_room
--- spec/component_previews/aeon/reading_room_brief_component_preview.rb
+++ spec/component_previews/aeon/reading_room_brief_component_preview.rb
@@ -8,25 +8,25 @@ module Aeon
     def spec
       @reading_room = Aeon::ReadingRoom.find_by(site: 'SPECUA')
 
-      render Aeon::ReadingRoomBriefComponent.new(reading_room: @reading_room)
+      render Aeon::ReadingRoomBriefComponent.new(reading_room: @reading_room, create_appointment: true)
     end
 
     def rumsey
       @reading_room = Aeon::ReadingRoom.find_by(site: 'RUMSEY')
 
-      render Aeon::ReadingRoomBriefComponent.new(reading_room: @reading_room)
+      render Aeon::ReadingRoomBriefComponent.new(reading_room: @reading_room, create_appointment: true)
     end
 
     def eal
       @reading_room = Aeon::ReadingRoom.find_by(site: 'EASTASIA')
 
-      render Aeon::ReadingRoomBriefComponent.new(reading_room: @reading_room)
+      render Aeon::ReadingRoomBriefComponent.new(reading_room: @reading_room, create_appointment: true)
     end
 
     def ars
       @reading_room = Aeon::ReadingRoom.find_by(site: 'ARS')
 
-      render Aeon::ReadingRoomBriefComponent.new(reading_room: @reading_room)
+      render Aeon::ReadingRoomBriefComponent.new(reading_room: @reading_room, create_appointment: true)
     end
     # @!endgroup
   end
