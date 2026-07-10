9a3a445c7d3a9918066891aec430d1213e1af730
Steve Taylor <sctaylor@stanford.edu>
2026-07-10 16:21:57 -0700

    Use a large modal for the feedback form

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -4,14 +4,17 @@
 class ModalComponent < ViewComponent::Base
   attr_reader :id, :data
 
+  SIZES = { sm: 'modal-sm', lg: 'modal-lg', xl: 'modal-xl' }.freeze
+
   renders_one :title
   renders_one :banner
   renders_one :custom_header, ->(classes: %w[modal-header fw-normal], &block) { tag.div(class: classes, &block) }
   renders_one :body, ->(classes: %w[modal-body fw-normal], &block) { tag.div(class: classes, &block) }
   renders_one :footer, ->(classes: %w[modal-footer], &block) { tag.div(class: classes, &block) }
 
-  def initialize(id:, classes: %w[modal fade], header_classes: [], data: {})
+  def initialize(id:, size: nil, classes: %w[modal fade], header_classes: [], data: {})
     @id = id
+    @size = size
     @data = data
     @classes = classes
     @header_classes = header_classes
@@ -19,5 +22,9 @@ class ModalComponent < ViewComponent::Base
 
   def title_id
     "title-#{id}"
+  end
+
+  def dialog_classes
+    ['modal-dialog', 'modal-dialog-centered', 'modal-dialog-scrollable', SIZES[@size]].compact
   end
 end
