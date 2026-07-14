18e66c632523bc486bdf4c2e1b82648042d4be36
Chris Beer <cabeer@stanford.edu>
2026-07-14 12:55:45 -0700

    Extract Folio::ItemSelectorComponent

---
+++ app/components/folio/item_selector_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Folio
+  # Component for rendering an item selector for an instance
+  class ItemSelectorComponent < ViewComponent::Base
+    attr_reader :f
+
+    def initialize(f:) # rubocop:disable Naming/MethodParameterName
+      @f = f
+      super()
+    end
+  end
+end
