986b0e502d54905a391a2be7733dc3e95d706434
Chris Beer <cabeer@stanford.edu>
2026-07-15 09:33:30 -0700

    Ensure we're getting the flat card styling (e.g. no shadow)

--- app/components/home/flat_card_component.rb
+++ app/components/home/flat_card_component.rb
@@ -3,9 +3,9 @@
 module Home
   # Flat card preset (no shadow, no border, no outer margin) used by home page cards.
   class FlatCardComponent < ::CardComponent
-    def initialize(**)
+    def initialize(classes: [], **)
       super(
-        classes: %w[shadow-none border-0 rounded-1],
+        classes: classes + %w[shadow-none border-0 rounded-1],
         header_classes: %w[border-bottom-0],
         margin_class: nil,
         **
