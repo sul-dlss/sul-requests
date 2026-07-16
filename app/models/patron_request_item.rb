dc840c4f3c06231c103ac26bc3d199151d773b45
Chris Beer <cabeer@stanford.edu>
2026-07-16 13:34:23 -0700

    Record the item id, barcode and callnumber in the database

--- app/models/patron_request_item.rb
+++ app/models/patron_request_item.rb
@@ -13,9 +13,28 @@ class PatronRequestItem < ApplicationRecord
     :title, :hierarchy, :for_publication, :requested_pages, :additional_information, :appointment_id, :activity_ids
   ], coder: JSON
 
+  after_initialize :update_item_metadata, if: -> { item_id.blank? || @barcode_or_item_id.present? }
+  attr_writer :barcode_or_item_id
+
+  def barcode_or_item_id
+    @barcode_or_item_id || barcode || item_id
+  end
+
+  def update_item_metadata # rubocop:disable Metrics/AbcSize
+    @folio_item = patron_request.folio_instance&.items&.find do |i|
+      i.barcode == barcode_or_item_id || i.id == barcode_or_item_id
+    end
+
+    self.barcode = barcode_or_item_id and return if @folio_item.blank?
+
+    self.item_id = @folio_item.id
+    self.barcode = @folio_item.barcode
+    self.item_callnumber = @folio_item.callnumber
+  end
+
   def folio_item
     return if ead_url
 
-    @folio_item ||= patron_request.folio_instance.items.find { |i| i.id == item_id }
+    @folio_item ||= patron_request.folio_instance.items.find { |i| i.id == item_id || i.barcode == barcode || i.id == barcode }
   end
 end
