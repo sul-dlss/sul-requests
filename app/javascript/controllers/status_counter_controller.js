a9fa29aec733415e15af153b51422f9ceb686567
Chris Beer <cabeer@stanford.edu>
2026-07-27 10:22:26 -0700

    Show 'saved for later' text instead of '0 ready'

--- app/javascript/controllers/status_counter_controller.js
+++ app/javascript/controllers/status_counter_controller.js
@@ -10,11 +10,13 @@ export default class extends Controller {
   update(event) {
     if (!this.hasCounterTarget) return
     const completeCount = this.element.querySelectorAll('[data-selected-item-form-status-value="complete"]').length;
+    const incompleteCount = this.element.querySelectorAll('[data-selected-item-form-status-value="incomplete"]').length;
     const savedCount = this.element.querySelectorAll('[data-saved-for-later]').length;
 
     let text = `${completeCount} ready to submit`;
-    if (savedCount > 0) {
-      text += ` · ${savedCount} saving for later`;
+
+    if (completeCount == 0 && incompleteCount == 0 && savedCount > 0) {
+      text = `${savedCount} saved for later`;
     }
 
     this.counterTarget.innerHTML = text;
