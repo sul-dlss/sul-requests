020e848a9c1652cc911c799f7b94245aaf221d0c
Chris Beer <cabeer@stanford.edu>
2026-07-29 14:26:41 -0700

    Only use the first word as the fine status.

--- app/components/folio/fine_status_pill_component.rb
+++ app/components/folio/fine_status_pill_component.rb
@@ -5,8 +5,6 @@ module Folio
   class FineStatusPillComponent < ViewComponent::Base
     attr_reader :fine
 
-    delegate :status, to: :fine
-
     def initialize(fine:)
       @fine = fine
       super()
@@ -17,20 +15,20 @@ module Folio
     end
 
     def status_classes
-      case fine_status
+      case status.upcase
       when 'PAID'
-        %w[fine-status bg-green text-green]
+        %w[fine-status bg-green text-green text-uppercase]
       else
-        %w[fine-status bg-stanford-20-black text-black]
+        %w[fine-status bg-stanford-20-black text-black text-uppercase]
       end
     end
 
-    def fine_status
-      status.gsub('fully', '').strip.upcase
+    def status
+      fine.status.split(' ', 2).first
     end
 
     def call
-      render PillComponent.new(classes: status_classes).with_content(fine_status)
+      render PillComponent.new(classes: status_classes).with_content(status)
     end
   end
 end
