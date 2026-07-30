26ebbf0e61a2807a6374a19ae78f2a4cbcb010b7
Chris Beer <cabeer@stanford.edu>
2026-07-30 08:31:43 -0700

    Extract System::GroupedRequestsComponent

---
+++ app/components/system/grouped_requests_layout_component.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module System
+  # Render a request grouping
+  class GroupedRequestsLayoutComponent < ViewComponent::Base
+    renders_one :type
+    renders_one :details
+    renders_one :status_message
+
+    renders_one :title
+    renders_one :record_header
+
+    renders_one :cover_image
+    renders_many :requests
+
+    attr_reader :id, :element, :attr
+
+    def initialize(id:, element: 'li', show_more: false, sortable: false, **attr)
+      @id = id
+      @element = element
+      @attr = attr
+
+      @sortable = sortable
+      @show_more = show_more
+    end
+
+    def sortable?
+      @sortable
+    end
+
+    def show_more?
+      @show_more
+    end
+  end
+end
