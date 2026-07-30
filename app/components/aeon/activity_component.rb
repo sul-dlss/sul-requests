b8ae5906ab258dc44e422ac0b3e8b19243085abf
Chris Beer <cabeer@stanford.edu>
2026-07-29 17:01:21 -0700

    Render the activity's requests when updating.

--- app/components/aeon/activity_component.rb
+++ app/components/aeon/activity_component.rb
@@ -5,8 +5,13 @@ module Aeon
   class ActivityComponent < ViewComponent::Base
     attr_reader :activity
 
-    def initialize(activity:)
+    def initialize(activity:, requests: false)
       @activity = activity
+      @requests = requests
+    end
+
+    def requests?
+      @requests
     end
   end
 end
