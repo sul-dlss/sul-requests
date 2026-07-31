a04586c3aa5179c30320cc2c1fbaf2a6879e0e3b
Chris Beer <cabeer@stanford.edu>
2026-07-31 09:14:26 -0700

    Implement client-side activity sort without extracting sortable components.

--- app/controllers/aeon_activities_controller.rb
+++ app/controllers/aeon_activities_controller.rb
@@ -4,7 +4,6 @@
 #  Controller for displaying Aeon activites for a user
 ###
 class AeonActivitiesController < ApplicationController
-  ALLOWED_SORTS = %w[sort_key name activity_type].freeze
   include AeonController
 
   before_action :authorize_activity
@@ -21,24 +20,6 @@ class AeonActivitiesController < ApplicationController
   end
 
   def load_activities
-    @activities = all_activities
-    filter
-    sort_results
-  end
-
-  def filter
-    return unless params[:filter]
-
-    @activities = @activities.select { |activity| activity.activity_type == params[:filter] }
-  end
-
-  def sort_results
-    sort = params[:sort].presence_in(ALLOWED_SORTS) || 'sort_key'
-    sort_keys = ([sort] + ALLOWED_SORTS).compact.uniq
-    @activities = @activities.sort_by { |obj| sort_keys.map { |k| obj.public_send(k) } }
-  end
-
-  def all_activities
-    @all_activities ||= current_user.aeon.activities
+    @activities = current_user.aeon.activities
   end
 end
