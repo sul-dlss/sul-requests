c4b5999308548e79e2e86233965877c67e06f45d
Chris Beer <cabeer@stanford.edu>
2026-09-24 09:45:53 -0700

    Abandon requests-dev for mylibrary-dev.

--- config/deploy/mylib_dev.rb
+++
@@ -1,9 +0,0 @@
-set :rails_env, 'production'
-set :application, 'mylibrary'
-set :deploy_to, '/opt/app/mylibrary/mylibrary'
-
-server 'mylibrary-dev.stanford.edu', user: 'mylibrary', roles: %w(web db app)
-
-set :bundle_without, %w{deployment development test}.join(' ')
-
-Capistrano::OneTimeKey.generate_one_time_key!
